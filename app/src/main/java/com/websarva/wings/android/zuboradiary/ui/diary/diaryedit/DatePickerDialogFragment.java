8e967209c11caeecd1244d787d0ed33b967ecee5
Takuya USAMI <tusami@gmail.com>
2024-09-14 00:21:22 +0900

    DatePickerDialogにThemeColorを適応させたかったが保留

--- app/src/main/java/com/websarva/wings/android/zuboradiary/ui/diary/diaryedit/DatePickerDialogFragment.java
+++ app/src/main/java/com/websarva/wings/android/zuboradiary/ui/diary/diaryedit/DatePickerDialogFragment.java
@@ -6,22 +6,32 @@ import android.app.Dialog;
 import android.os.Bundle;
 import android.widget.DatePicker;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.DialogFragment;
 import androidx.lifecycle.SavedStateHandle;
 import androidx.navigation.NavBackStackEntry;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 
+import com.google.android.material.datepicker.MaterialDatePicker;
+import com.squareup.moshi.ToJson;
+
 import java.time.LocalDate;
+import java.util.Date;
+import java.util.Objects;
+
+import dagger.internal.Preconditions;
 
 public class DatePickerDialogFragment extends DialogFragment{
     private static final String fromClassName = "From" + DatePickerDialogFragment.class.getName();
     public static final String KEY_SELECTED_DATE = "SelectedDate" + fromClassName;
 
+    @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         LocalDate currentDate =
                 DatePickerDialogFragmentArgs.fromBundle(requireArguments()).getCurrentDate();
+        // TODO:ThemeColor保留
         return new DatePickerDialog(
                 requireContext(),
                 new DatePickerDialog.OnDateSetListener() {
