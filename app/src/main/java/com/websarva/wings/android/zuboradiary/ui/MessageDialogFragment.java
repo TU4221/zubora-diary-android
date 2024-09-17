07ab0e534181d95477f4a1828579d15ec90ddb76
Takuya USAMI <tusami@gmail.com>
2024-09-17 15:53:59 +0900

    MessageDialogにBaseAlertDialogFragmentを実装。

--- app/src/main/java/com/websarva/wings/android/zuboradiary/ui/MessageDialogFragment.java
+++ app/src/main/java/com/websarva/wings/android/zuboradiary/ui/MessageDialogFragment.java
@@ -2,6 +2,7 @@ package com.websarva.wings.android.zuboradiary.ui;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -10,35 +11,40 @@ import androidx.fragment.app.DialogFragment;
 
 import com.websarva.wings.android.zuboradiary.R;
 
-public class MessageDialogFragment extends DialogFragment {
-    @NonNull
+public class MessageDialogFragment extends BaseAlertDialogFragment {
+
     @Override
-    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
-        String title =
-                MessageDialogFragmentArgs.fromBundle(requireArguments()).getTitle();
-        builder.setTitle(title);
+    protected String createTitle() {
+        return MessageDialogFragmentArgs.fromBundle(requireArguments()).getTitle();
+    }
 
-        String message =
-                MessageDialogFragmentArgs.fromBundle(requireArguments()).getMessage();
-        builder.setMessage(message);
-        builder.setPositiveButton(
-                R.string.dialog_diary_item_title_delete_confirmation_yes, null);
+    @Override
+    protected String createMessage() {
+        return MessageDialogFragmentArgs.fromBundle(requireArguments()).getMessage();
+    }
 
-        AlertDialog dialog = builder.create();
+    @Override
+    protected void handlePositiveButton(@NonNull DialogInterface dialog, int which) {
+        // 処理なし
+    }
 
-        // TODO:コメントが理解できないので動作を確認してコメント修正
-        // MEMO:ダイアログフラグメントのCANCEL・DISMISS 処理について、
-        //      このクラスのような、DialogFragmentにAlertDialogを作成する場合、
-        //      CANCEL・DISMISSの処理内容はDialogFragmentのonCancel/onDismissをオーバーライドする必要がある。
-        //      DialogFragment、AlertDialogのリスナセットメソッドを使用して処理内容を記述きても処理はされない。
-        //      UIに表示されているダイアログ外の部分をタッチしてダイアログを閉じる(キャンセルする)ことができるが、
-        //      これを無効にするには、AlertDialog#setCanceledOnTouchOutsideを設定する必要あり。
-        //      またスマホ等の戻るボタンでもダイアログを閉じる(キャンセルする)ことは可能だが、
-        //      これを無効にするには、DialogFragment#setCancelableを設定する必要あり。
-        dialog.setCanceledOnTouchOutside(true);
-        this.setCancelable(true);
+    @Override
+    protected void handleNegativeButton(@NonNull DialogInterface dialog, int which) {
+        // 処理なし
+    }
 
-        return dialog;
+    @Override
+    public boolean isCancelableOtherThanPressingButton() {
+        return true;
+    }
+
+    @Override
+    protected void handleCancel(@NonNull DialogInterface dialog) {
+        // 処理なし
+    }
+
+    @Override
+    protected void handleDismiss() {
+        // 処理なし
     }
 }
