093c2c113bfd5526b99ea61a270a12b2c004aa82
Takuya USAMI <tusami@gmail.com>
2024-09-17 15:01:09 +0900

    DiaryItemTitleEdit AlertDialogにBaseAlertDialogFragmentを実装

--- app/src/main/java/com/websarva/wings/android/zuboradiary/ui/diary/diaryitemtitleedit/DiaryItemTitleDeleteConfirmationDialogFragment.java
+++ app/src/main/java/com/websarva/wings/android/zuboradiary/ui/diary/diaryitemtitleedit/DiaryItemTitleDeleteConfirmationDialogFragment.java
@@ -12,83 +12,62 @@ import androidx.lifecycle.SavedStateHandle;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.websarva.wings.android.zuboradiary.R;
+import com.websarva.wings.android.zuboradiary.ui.BaseAlertDialogFragment;
 
-public class DiaryItemTitleDeleteConfirmationDialogFragment extends DialogFragment {
+public class DiaryItemTitleDeleteConfirmationDialogFragment extends BaseAlertDialogFragment {
+
     private static final String fromClassName =
             "From" + DiaryItemTitleDeleteConfirmationDialogFragment.class.getName();
     public static final String KEY_SELECTED_BUTTON = "SelectedButton" + fromClassName;
     public static final String KEY_DELETE_LIST_ITEM_POSITION = "DeleteItemPosition" + fromClassName;
 
-    @NonNull
     @Override
-    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
-        builder.setTitle(R.string.dialog_diary_item_title_delete_confirmation_title);
+    protected String createTitle() {
+        return getString(R.string.dialog_diary_item_title_delete_confirmation_title);
+    }
 
+    @Override
+    protected String createMessage() {
         String deleteItemTitle =
                 DiaryItemTitleDeleteConfirmationDialogFragmentArgs
                         .fromBundle(requireArguments()).getDeleteItemTitle();
-        String message = getString(R.string.dialog_diary_item_title_delete_confirmation_first_message) + deleteItemTitle + getString(
-                        R.string.dialog_diary_item_title_delete_confirmation_second_message);
-
-        builder.setMessage(message);
-        builder.setPositiveButton(
-                R.string.dialog_diary_item_title_delete_confirmation_yes,
-                new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        processResults(DialogInterface.BUTTON_POSITIVE);
-                    }
-                }
-        );
-        builder.setNegativeButton(
-                R.string.dialog_diary_item_title_delete_confirmation_no,
-                new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        processResults(DialogInterface.BUTTON_NEGATIVE);
-                    }
-                }
-        );
-        AlertDialog dialog = builder.create();
-
-        // TODO:コメントが理解できないので動作を確認してコメント修正
-        // MEMO:ダイアログフラグメントのCANCEL・DISMISS 処理について、
-        //      このクラスのような、DialogFragmentにAlertDialogを作成する場合、
-        //      CANCEL・DISMISSの処理内容はDialogFragmentのonCancel/onDismissをオーバーライドする必要がある。
-        //      DialogFragment、AlertDialogのリスナセットメソッドを使用して処理内容を記述きても処理はされない。
-        //      UIに表示されているダイアログ外の部分をタッチしてダイアログを閉じる(キャンセルする)ことができるが、
-        //      これを無効にするには、AlertDialog#setCanceledOnTouchOutsideを設定する必要あり。
-        //      またスマホ等の戻るボタンでもダイアログを閉じる(キャンセルする)ことは可能だが、
-        //      これを無効にするには、DialogFragment#setCancelableを設定する必要あり。
-        dialog.setCanceledOnTouchOutside(true);
-        this.setCancelable(true);
-
-        return dialog;
+        return getString(R.string.dialog_diary_item_title_delete_confirmation_first_message) + deleteItemTitle + getString(
+                R.string.dialog_diary_item_title_delete_confirmation_second_message);
     }
 
-    // ダイアログ枠外タッチ、popBackStack時に処理
     @Override
-    public void onCancel (DialogInterface dialog) {
-        super.onCancel(dialog);
+    protected void handlePositiveButton(@NonNull DialogInterface dialog, int which) {
+        processResults(DialogInterface.BUTTON_POSITIVE);
+    }
+
+    @Override
+    protected void handleNegativeButton(@NonNull DialogInterface dialog, int which) {
         processResults(DialogInterface.BUTTON_NEGATIVE);
     }
 
-    // ダイアログ消失時に処理
     @Override
-    public void onDismiss (DialogInterface dialog) {
-        super.onDismiss(dialog);
+    public boolean isCancelable() {
+        return true;
+    }
+
+    @Override
+    protected void handleCancel(@NonNull DialogInterface dialog) {
+        processResults(DialogInterface.BUTTON_NEGATIVE);
     }
 
     private void processResults(int status) {
-        NavController navController = NavHostFragment.findNavController(this);
-        SavedStateHandle savedStateHandle =
-                navController.getPreviousBackStackEntry().getSavedStateHandle();
-        savedStateHandle.set(KEY_SELECTED_BUTTON, status);
+        setResult(KEY_SELECTED_BUTTON, status);
+
         int deleteListItemPosition =
                 DiaryItemTitleDeleteConfirmationDialogFragmentArgs
                         .fromBundle(getArguments()).getDeleteListItemPosition();
-        savedStateHandle.set(KEY_DELETE_LIST_ITEM_POSITION, deleteListItemPosition);
+        setResult(KEY_DELETE_LIST_ITEM_POSITION, deleteListItemPosition);
+    }
+
+    @Override
+    protected void handleDismiss() {
+
     }
 }
