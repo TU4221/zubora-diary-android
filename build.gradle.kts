9e28b93ee7a66ebc1959bb678e1d7e5727995a07
Takuya USAMI <tusami@gmail.com>
2025-01-22 16:07:24 +0900

    Kotlin Verアップグレード(1.8.10 -> 2.0.21)

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.5.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
 
     // RoomのSchemaエクスポート機能を使用する為に追加
     id("androidx.room") version "2.6.1" apply false
@@ -13,7 +13,7 @@ plugins {
 
     // KSP機能追加
     // https://developer.android.com/build/migrate-to-ksp?hl=ja
-    id("com.google.devtools.ksp") version "1.8.10-1.0.9" apply false // kotlin Ver.1.8.10
+    id("com.google.devtools.ksp") version "2.0.21-1.0.27" apply false // kotlin Ver.1.8.10
 }
 buildscript {
     dependencies {
