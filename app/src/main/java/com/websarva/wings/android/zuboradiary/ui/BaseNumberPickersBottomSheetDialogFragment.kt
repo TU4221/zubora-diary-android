fa5fa884012d6e875e08faad577b7f300b14b2f3
Takuya USAMI <tusami@gmail.com>
2025-01-27 16:27:38 +0900

    Java -> Kotlin 変換 (BaseNumberPickersBottomSheetDialogFragment)

--- app/src/main/java/com/websarva/wings/android/zuboradiary/ui/BaseNumberPickersBottomSheetDialogFragment.kt
+++ app/src/main/java/com/websarva/wings/android/zuboradiary/ui/BaseNumberPickersBottomSheetDialogFragment.kt
@@ -1,87 +1,70 @@
-package com.websarva.wings.android.zuboradiary.ui;
+package com.websarva.wings.android.zuboradiary.ui
 
-import android.content.Context;
-import android.os.Build;
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.content.Context
+import android.os.Build
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.view.ContextThemeWrapper
+import androidx.navigation.fragment.NavHostFragment
+import com.websarva.wings.android.zuboradiary.databinding.DialogFragmentNumberPickersBinding
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.view.ContextThemeWrapper;
-import androidx.lifecycle.SavedStateHandle;
-import androidx.navigation.NavBackStackEntry;
-import androidx.navigation.NavController;
-import androidx.navigation.fragment.NavHostFragment;
+abstract class BaseNumberPickersBottomSheetDialogFragment : BaseBottomSheetDialogFragment() {
 
-import com.websarva.wings.android.zuboradiary.data.preferences.ThemeColor;
-import com.websarva.wings.android.zuboradiary.databinding.DialogFragmentNumberPickersBinding;
-
-import java.util.Objects;
-
-public abstract class BaseNumberPickersBottomSheetDialogFragment extends BaseBottomSheetDialogFragment {
     // View関係
-    protected DialogFragmentNumberPickersBinding binding;
+    protected lateinit var binding: DialogFragmentNumberPickersBinding
 
-    @Override
-    @NonNull
-    protected final View createDialogView(
-            @NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-
-        binding = createBinding(inflater, container);
-        binding.buttonDecision.setOnClickListener(new PositiveButtonClickListener());
-        binding.buttonCancel.setOnClickListener(new NegativeButtonClickListener());
-        setUpNumberPickers(binding);
-        return binding.getRoot();
+    override fun createDialogView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        binding = createBinding(inflater, container)
+        binding.buttonDecision.setOnClickListener(PositiveButtonClickListener())
+        binding.buttonCancel.setOnClickListener(NegativeButtonClickListener())
+        setUpNumberPickers(binding)
+        return binding.root
     }
 
-    @NonNull
-    private DialogFragmentNumberPickersBinding createBinding(
-            @NonNull LayoutInflater inflater, @Nullable ViewGroup container) {
-
+    private fun createBinding(
+        inflater: LayoutInflater, container: ViewGroup?
+    ): DialogFragmentNumberPickersBinding {
         // HACK:下記理由から、ThemeColor#getNumberPickerBottomSheetDialogThemeResId()から
         //      ThemeResIdを取得してInflaterを再作成。
         //      ・NumberPickerの値はThemeが適用されず、TextColorはApiLevel29以上からしか変更できない。
         //      ・ThemeColorBlackの時は背景が黒となり、NumberPickerの値が見えない。
-        ThemeColor themeColor = requireThemeColor();
-        int themeResId = themeColor.getNumberPickerBottomSheetDialogThemeResId();
-        Context contextWithTheme = new ContextThemeWrapper(requireActivity(), themeResId);
-        LayoutInflater _inflater = inflater.cloneInContext(contextWithTheme);
 
-        DialogFragmentNumberPickersBinding binding =
-                DialogFragmentNumberPickersBinding.inflate(_inflater, container, false);
+        val themeColor = requireThemeColor()
+        val themeResId = themeColor.numberPickerBottomSheetDialogThemeResId
+        val contextWithTheme: Context = ContextThemeWrapper(requireActivity(), themeResId)
+        val cloneInflater = inflater.cloneInContext(contextWithTheme)
 
-        setUpNumberPickerTextColor(binding);
+        val binding =
+            DialogFragmentNumberPickersBinding.inflate(cloneInflater, container, false)
 
-        return binding;
+        setUpNumberPickerTextColor(binding)
+
+        return binding
     }
 
-    private void setUpNumberPickerTextColor(DialogFragmentNumberPickersBinding binding) {
-        Objects.requireNonNull(binding);
-
+    private fun setUpNumberPickerTextColor(binding: DialogFragmentNumberPickersBinding) {
         if (Build.VERSION.SDK_INT >= 29) {
-            ThemeColor themeColor = requireThemeColor();
-            int onSurfaceVariantColor = themeColor.getOnSurfaceVariantColor(getResources());
-            binding.numberPickerFirst.setTextColor(onSurfaceVariantColor);
-            binding.numberPickerSecond.setTextColor(onSurfaceVariantColor);
-            binding.numberPickerThird.setTextColor(onSurfaceVariantColor);
+            val themeColor = requireThemeColor()
+            val onSurfaceVariantColor = themeColor.getOnSurfaceVariantColor(resources)
+            binding.numberPickerFirst.textColor = onSurfaceVariantColor
+            binding.numberPickerSecond.textColor = onSurfaceVariantColor
+            binding.numberPickerThird.textColor = onSurfaceVariantColor
         }
     }
 
     /**
      * BaseNumberPickersBottomSheetDialogFragment#createDialogView()で呼び出される。
-     * */
-    protected abstract void setUpNumberPickers(DialogFragmentNumberPickersBinding binding);
+     */
+    protected abstract fun setUpNumberPickers(binding: DialogFragmentNumberPickersBinding?)
 
-    protected final void setResult(String key, Object value) {
-        Objects.requireNonNull(key);
-        Objects.requireNonNull(value);
-
-        NavController navController = NavHostFragment.findNavController(this);
-        NavBackStackEntry navBackStackEntry = navController.getPreviousBackStackEntry();
-        Objects.requireNonNull(navBackStackEntry);
-        SavedStateHandle savedStateHandle = navBackStackEntry.getSavedStateHandle();
-        savedStateHandle.set(key, value);
+    protected fun setResult(key: String, value: Any) {
+        val navController = NavHostFragment.findNavController(this)
+        val navBackStackEntry = requireNotNull(navController.previousBackStackEntry)
+        val savedStateHandle = navBackStackEntry.savedStateHandle
+        savedStateHandle[key] = value
     }
 }
