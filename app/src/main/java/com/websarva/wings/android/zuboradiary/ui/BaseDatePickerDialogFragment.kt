bc0a952400ea6409d95e4c82953fd5803a90e7e2
Takuya USAMI <tusami@gmail.com>
2025-01-27 16:16:03 +0900

    Java -> Kotlin 変換 (BaseDatePickerDialogFragment)

--- app/src/main/java/com/websarva/wings/android/zuboradiary/ui/BaseDatePickerDialogFragment.kt
+++ app/src/main/java/com/websarva/wings/android/zuboradiary/ui/BaseDatePickerDialogFragment.kt
@@ -1,121 +1,95 @@
-package com.websarva.wings.android.zuboradiary.ui;
+package com.websarva.wings.android.zuboradiary.ui
+
+import android.app.Dialog
+import android.content.DialogInterface
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.DialogFragment
+import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.datepicker.MaterialDatePicker
+import com.websarva.wings.android.zuboradiary.data.preferences.ThemeColor
+import com.websarva.wings.android.zuboradiary.ui.settings.SettingsViewModel
+import java.time.Instant
+import java.time.LocalDate
+import java.time.ZoneId
 
 
-import android.app.Dialog;
-import android.content.DialogInterface;
-import android.os.Bundle;
-import android.view.View;
+abstract class BaseDatePickerDialogFragment : DialogFragment() {
+    protected lateinit var settingsViewModel: SettingsViewModel
 
-import androidx.annotation.NonNull;
-import androidx.fragment.app.DialogFragment;
-import androidx.lifecycle.ViewModelProvider;
-
-import com.google.android.material.datepicker.MaterialDatePicker;
-import com.websarva.wings.android.zuboradiary.data.preferences.ThemeColor;
-import com.websarva.wings.android.zuboradiary.ui.settings.SettingsViewModel;
-
-import java.time.Instant;
-import java.time.LocalDate;
-import java.time.ZoneId;
-import java.util.Objects;
-
-public abstract class BaseDatePickerDialogFragment extends DialogFragment{
-
-    protected SettingsViewModel settingsViewModel;
-
-    @NonNull
-    @Override
-    public Dialog onCreateDialog(Bundle savedInstanceState) {
-
-        settingsViewModel = createSettingsViewModel();
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        settingsViewModel = createSettingsViewModel()
 
         // MEMO:MaterialDatePickerはDialogクラスを作成できないのでダミーDialogを作成して戻り値として返し
         //      MaterialDatePicker#show()でDatePickerDialogを表示する。ダミーDialogも重なって表示されるので、
         //      MaterialDatePickerに追加したリスナーでダミーDialogを閉じる(Dialog#dismiss())。
-        Dialog dummyDialog = new Dialog(requireContext());
+        val dummyDialog = Dialog(requireContext())
 
-        MaterialDatePicker<Long> datePicker = createDatePickerDialog(dummyDialog);
-        datePicker.show(getChildFragmentManager(),"");
+        val datePicker = createDatePickerDialog(dummyDialog)
+        datePicker.show(childFragmentManager, "")
 
-        return dummyDialog;
+        return dummyDialog
     }
 
-    @NonNull
-    private SettingsViewModel createSettingsViewModel() {
-        ViewModelProvider provider = new ViewModelProvider(requireActivity());
-        SettingsViewModel settingsViewModel = provider.get(SettingsViewModel.class);
-        return Objects.requireNonNull(settingsViewModel);
+    private fun createSettingsViewModel(): SettingsViewModel {
+        val provider = ViewModelProvider(requireActivity())
+        return provider[SettingsViewModel::class.java]
     }
 
-    @NonNull
-    protected final ThemeColor requireThemeColor() {
-        return settingsViewModel.loadThemeColorSettingValue();
+    protected fun requireThemeColor(): ThemeColor {
+        return settingsViewModel.loadThemeColorSettingValue()
     }
 
-    @NonNull
-    private MaterialDatePicker<Long> createDatePickerDialog(Dialog dummyDialog) {
-        MaterialDatePicker.Builder<Long> builder = MaterialDatePicker.Builder.datePicker();
+    private fun createDatePickerDialog(dummyDialog: Dialog): MaterialDatePicker<Long> {
+        val builder = MaterialDatePicker.Builder.datePicker()
 
-        int themeResId = requireThemeColor().getDatePickerDialogThemeResId();
-        builder.setTheme(themeResId);
+        val themeResId = requireThemeColor().datePickerDialogThemeResId
+        builder.setTheme(themeResId)
 
-        LocalDate initialDate = createInitialDate();
-        Objects.requireNonNull(initialDate);
-        long initialEpochMilli =
-                initialDate.atStartOfDay(ZoneId.systemDefault()).toInstant().toEpochMilli();
-        builder.setSelection(initialEpochMilli);
+        val initialDate = createInitialDate()
+        val initialEpochMilli =
+            initialDate.atStartOfDay(ZoneId.systemDefault()).toInstant().toEpochMilli()
+        builder.setSelection(initialEpochMilli)
 
-        MaterialDatePicker<Long> datePicker = builder.build();
+        val datePicker = builder.build()
 
-        setUpOnClickListener(datePicker, dummyDialog);
+        setUpOnClickListener(datePicker, dummyDialog)
 
-        return datePicker;
+        return datePicker
     }
 
-    protected abstract LocalDate createInitialDate();
+    protected abstract fun createInitialDate(): LocalDate
 
-    private void setUpOnClickListener(MaterialDatePicker<Long> datePicker, Dialog dummyDialog) {
-        Objects.requireNonNull(datePicker);
-        Objects.requireNonNull(dummyDialog);
-
-        datePicker.addOnPositiveButtonClickListener(selection -> {
-            Objects.requireNonNull(selection);
-
+    private fun setUpOnClickListener(datePicker: MaterialDatePicker<Long>, dummyDialog: Dialog) {
+        datePicker.addOnPositiveButtonClickListener { selection: Long ->
             // 選択日付型変換(EpochMilli -> LocalDate)
-            Instant instant = Instant.ofEpochMilli(selection);
-            LocalDate selectedDate = LocalDate.ofInstant(instant, ZoneId.systemDefault());
-            Objects.requireNonNull(selectedDate);
-            handleOnPositiveButtonClick(selectedDate);
-            dummyDialog.dismiss();
-        });
+            val instant = Instant.ofEpochMilli(selection)
+            val selectedDate = LocalDate.ofInstant(instant, ZoneId.systemDefault())
+            handleOnPositiveButtonClick(selectedDate)
+            dummyDialog.dismiss()
+        }
 
-        datePicker.addOnNegativeButtonClickListener(v -> {
-            Objects.requireNonNull(v);
+        datePicker.addOnNegativeButtonClickListener { v: View ->
+            handleOnNegativeButtonClick(v)
+            dummyDialog.dismiss()
+        }
 
-            handleOnNegativeButtonClick(v);
-            dummyDialog.dismiss();
-        });
+        datePicker.addOnCancelListener { dialog: DialogInterface ->
+            handleOnCancel(dialog)
+            dummyDialog.dismiss()
+        }
 
-        datePicker.addOnCancelListener(dialog -> {
-            Objects.requireNonNull(dialog);
-
-            handleOnCancel(dialog);
-            dummyDialog.dismiss();
-        });
-
-        datePicker.addOnDismissListener(dialog -> {
-            Objects.requireNonNull(dialog);
-
-            handleOnDismiss(dialog);
-            dummyDialog.dismiss();
-        });
+        datePicker.addOnDismissListener { dialog: DialogInterface ->
+            handleOnDismiss(dialog)
+            dummyDialog.dismiss()
+        }
     }
 
-    protected abstract void handleOnPositiveButtonClick(@NonNull LocalDate selectedDate);
+    protected abstract fun handleOnPositiveButtonClick(selectedDate: LocalDate)
 
-    protected abstract void handleOnNegativeButtonClick(@NonNull View v);
+    protected abstract fun handleOnNegativeButtonClick(v: View)
 
-    protected abstract void handleOnCancel(@NonNull DialogInterface dialog);
+    protected abstract fun handleOnCancel(dialog: DialogInterface)
 
-    protected abstract void handleOnDismiss(@NonNull DialogInterface dialog);
+    protected abstract fun handleOnDismiss(dialog: DialogInterface)
 }
