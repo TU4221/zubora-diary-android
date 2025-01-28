18c6f69f7363357ba2f5a4ab3c3c02406b48e78d
Takuya USAMI <tusami@gmail.com>
2025-01-28 15:31:36 +0900

    BindingAdapters 静的メソッドアノテーション追加

--- app/src/main/java/com/websarva/wings/android/zuboradiary/ui/BindingAdapters.kt
+++ app/src/main/java/com/websarva/wings/android/zuboradiary/ui/BindingAdapters.kt
@@ -10,6 +10,7 @@ object BindingAdapters {
     //      対策として下記メソッド作成。
     //      (初めは"android:drawableStart"を使用していたが、IDEの警告より"app:drawableStartCompat"に変更。
     //       しかし、現状layoutの構成ではDrawable型の変数を代入したかった為、このような対策をとる。)
+    @JvmStatic
     @BindingAdapter("drawableStartCompat")
     fun setDrawableStartCompat(textView: TextView, drawable: Drawable?) {
         if (drawable == null) return
