eb299ce64e966202b9e522c8b1d94f51f150653b
Takuya USAMI <tusami@gmail.com>
2025-01-28 15:32:59 +0900

    Java -> Kotlin 変換 (ThemeColorInflaterCreator)

--- app/src/main/java/com/websarva/wings/android/zuboradiary/ui/ThemeColorInflaterCreator.kt
+++ app/src/main/java/com/websarva/wings/android/zuboradiary/ui/ThemeColorInflaterCreator.kt
@@ -1,33 +1,18 @@
-package com.websarva.wings.android.zuboradiary.ui;
+package com.websarva.wings.android.zuboradiary.ui
 
-import android.content.Context;
-import android.view.LayoutInflater;
+import android.content.Context
+import android.view.LayoutInflater
+import androidx.appcompat.view.ContextThemeWrapper
+import com.websarva.wings.android.zuboradiary.data.preferences.ThemeColor
 
-import androidx.appcompat.view.ContextThemeWrapper;
+class ThemeColorInflaterCreator(
+    private val context: Context,
+    private val inflater: LayoutInflater,
+    private val themeColor: ThemeColor) {
 
-import com.websarva.wings.android.zuboradiary.data.preferences.ThemeColor;
-
-import java.util.Objects;
-
-public class ThemeColorInflaterCreator {
-
-    private final Context context;
-    private final LayoutInflater inflater;
-    private final ThemeColor themeColor;
-
-    public ThemeColorInflaterCreator(Context context, LayoutInflater inflater, ThemeColor themeColor) {
-        Objects.requireNonNull(context);
-        Objects.requireNonNull(inflater);
-        Objects.requireNonNull(themeColor);
-
-        this.context = context;
-        this.inflater = inflater;
-        this.themeColor = themeColor;
-    }
-
-    public LayoutInflater create(){
-        int themeResId = themeColor.getThemeResId();
-        Context contextWithTheme = new ContextThemeWrapper(context, themeResId);
-        return inflater.cloneInContext(contextWithTheme);
+    fun create(): LayoutInflater {
+        val themeResId = themeColor.themeResId
+        val contextWithTheme: Context = ContextThemeWrapper(context, themeResId)
+        return inflater.cloneInContext(contextWithTheme)
     }
 }
