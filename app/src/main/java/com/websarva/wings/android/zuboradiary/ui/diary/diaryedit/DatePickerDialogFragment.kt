e32822d21a5d689c36006aeaad58247a40247db5
Takuya USAMI <tusami@gmail.com>
2025-01-30 11:04:43 +0900

    Java -> Kotlin 変換 (DatePickerDialogFragment)

--- app/src/main/java/com/websarva/wings/android/zuboradiary/ui/diary/diaryedit/DatePickerDialogFragment.kt
+++ app/src/main/java/com/websarva/wings/android/zuboradiary/ui/diary/diaryedit/DatePickerDialogFragment.kt
@@ -1,54 +1,42 @@
-package com.websarva.wings.android.zuboradiary.ui.diary.diaryedit;
+package com.websarva.wings.android.zuboradiary.ui.diary.diaryedit
+
+import android.content.DialogInterface
+import android.view.View
+import androidx.navigation.fragment.NavHostFragment
+import com.websarva.wings.android.zuboradiary.ui.BaseDatePickerDialogFragment
+import java.time.LocalDate
 
 
-import android.content.DialogInterface;
-import android.view.View;
+class DatePickerDialogFragment : BaseDatePickerDialogFragment() {
 
-import androidx.annotation.NonNull;
-import androidx.lifecycle.SavedStateHandle;
-import androidx.navigation.NavBackStackEntry;
-import androidx.navigation.NavController;
-import androidx.navigation.fragment.NavHostFragment;
-
-import com.websarva.wings.android.zuboradiary.ui.BaseDatePickerDialogFragment;
-
-import java.time.LocalDate;
-import java.util.Objects;
-
-public class DatePickerDialogFragment extends BaseDatePickerDialogFragment {
-
-    private static final String fromClassName = "From" + DatePickerDialogFragment.class.getName();
-    static final String KEY_SELECTED_DATE = "SelectedDate" + fromClassName;
-
-    @Override
-    protected LocalDate createInitialDate() {
-        return DatePickerDialogFragmentArgs.fromBundle(requireArguments()).getDate();
+    companion object {
+        private val fromClassName = "From" + DatePickerDialogFragment::class.java.name
+        @JvmField
+        val KEY_SELECTED_DATE: String = "SelectedDate$fromClassName"
     }
 
-    @Override
-    protected void handleOnPositiveButtonClick(@NonNull LocalDate selectedDate) {
+    override fun createInitialDate(): LocalDate {
+        return DatePickerDialogFragmentArgs.fromBundle(requireArguments()).date
+    }
+
+    override fun handleOnPositiveButtonClick(selectedDate: LocalDate) {
         // 選択日付を返す
-        NavController navController =
-                NavHostFragment.findNavController(DatePickerDialogFragment.this);
-        Objects.requireNonNull(navController);
-        NavBackStackEntry navBackStackEntry = navController.getPreviousBackStackEntry();
-        Objects.requireNonNull(navBackStackEntry);
-        SavedStateHandle savedStateHandle = navBackStackEntry.getSavedStateHandle();
-        savedStateHandle.set(KEY_SELECTED_DATE, selectedDate);
+        val navController =
+            NavHostFragment.findNavController(this@DatePickerDialogFragment)
+        val navBackStackEntry = checkNotNull(navController.previousBackStackEntry)
+        val savedStateHandle = navBackStackEntry.savedStateHandle
+        savedStateHandle[KEY_SELECTED_DATE] = selectedDate
     }
 
-    @Override
-    protected void handleOnNegativeButtonClick(@NonNull View v) {
+    override fun handleOnNegativeButtonClick(v: View) {
         // 処理なし
     }
 
-    @Override
-    protected void handleOnCancel(@NonNull DialogInterface dialog) {
+    override fun handleOnCancel(dialog: DialogInterface) {
         // 処理なし
     }
 
-    @Override
-    protected void handleOnDismiss(@NonNull DialogInterface dialog) {
+    override fun handleOnDismiss(dialog: DialogInterface) {
         // 処理なし
     }
 }
