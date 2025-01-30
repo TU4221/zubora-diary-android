f9504000e9ba2b81e1f21015d5884d08c33a1a7d
Takuya USAMI <tusami@gmail.com>
2025-01-30 20:14:05 +0900

    Java -> Kotlin 変換 (SelectionHistoryListItem)

--- app/src/main/java/com/websarva/wings/android/zuboradiary/ui/diary/diaryitemtitleedit/SelectionHistoryListItem.kt
+++ app/src/main/java/com/websarva/wings/android/zuboradiary/ui/diary/diaryitemtitleedit/SelectionHistoryListItem.kt
@@ -1,32 +1,9 @@
-package com.websarva.wings.android.zuboradiary.ui.diary.diaryitemtitleedit;
+package com.websarva.wings.android.zuboradiary.ui.diary.diaryitemtitleedit
 
-import androidx.annotation.NonNull;
+import com.websarva.wings.android.zuboradiary.data.database.DiaryItemTitleSelectionHistoryItemEntity
+import java.time.LocalDateTime
 
-import com.websarva.wings.android.zuboradiary.data.database.DiaryItemTitleSelectionHistoryItemEntity;
-
-import java.time.LocalDateTime;
-import java.util.Objects;
-
-class SelectionHistoryListItem {
-
-    private final String title;
-    private final LocalDateTime log;
-
-    SelectionHistoryListItem(DiaryItemTitleSelectionHistoryItemEntity item) {
-        Objects.requireNonNull(item);
-
-        title = item.getTitle();
-        log = LocalDateTime.parse(item.getLog());
-    }
-
-
-    @NonNull
-    String getTitle() {
-        return title;
-    }
-
-    @NonNull
-    LocalDateTime getLog() {
-        return log;
-    }
+internal class SelectionHistoryListItem(item: DiaryItemTitleSelectionHistoryItemEntity) {
+    val title: String = item.title
+    val log: LocalDateTime = LocalDateTime.parse(item.log)
 }
