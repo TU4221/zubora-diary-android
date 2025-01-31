0046e362a1073e2aa43494087e1a1f06236c394f
Takuya USAMI <tusami@gmail.com>
2025-01-31 14:28:28 +0900

    Java -> Kotlin 変換 (AllSettingsInitializationDialogFragment)

--- app/src/main/java/com/websarva/wings/android/zuboradiary/ui/settings/AllSettingsInitializationDialogFragment.kt
+++ app/src/main/java/com/websarva/wings/android/zuboradiary/ui/settings/AllSettingsInitializationDialogFragment.kt
@@ -1,50 +1,42 @@
-package com.websarva.wings.android.zuboradiary.ui.settings;
+package com.websarva.wings.android.zuboradiary.ui.settings
 
-import android.content.DialogInterface;
+import android.content.DialogInterface
+import com.websarva.wings.android.zuboradiary.R
+import com.websarva.wings.android.zuboradiary.ui.BaseAlertDialogFragment
 
-import androidx.annotation.NonNull;
+class AllSettingsInitializationDialogFragment : BaseAlertDialogFragment() {
 
-import com.websarva.wings.android.zuboradiary.R;
-import com.websarva.wings.android.zuboradiary.ui.BaseAlertDialogFragment;
-
-public class AllSettingsInitializationDialogFragment extends BaseAlertDialogFragment {
-
-    private static final String FROM_CLASS_NAME =
-            "From" + AllSettingsInitializationDialogFragment.class.getName();
-    static final String KEY_SELECTED_BUTTON = "SelectedButton" + FROM_CLASS_NAME;
-
-    @Override
-    protected String createTitle() {
-        return getString(R.string.dialog_all_settings_initialization_title);
+    companion object {
+        private val FROM_CLASS_NAME =
+            "From" + AllSettingsInitializationDialogFragment::class.java.name
+        @JvmField
+        val KEY_SELECTED_BUTTON: String = "SelectedButton$FROM_CLASS_NAME"
     }
 
-    @Override
-    protected String createMessage() {
-        return getString(R.string.dialog_all_settings_initialization_message);
+    override val isCancelableOtherThanPressingButton: Boolean
+        get() = true
+
+    override fun createTitle(): String {
+        return getString(R.string.dialog_all_settings_initialization_title)
     }
 
-    @Override
-    protected void handleOnPositiveButtonClick(@NonNull DialogInterface dialog, int which) {
-        setResult(KEY_SELECTED_BUTTON, DialogInterface.BUTTON_POSITIVE);
+    override fun createMessage(): String {
+        return getString(R.string.dialog_all_settings_initialization_message)
     }
 
-    @Override
-    protected void handleOnNegativeButtonClick(@NonNull DialogInterface dialog, int which) {
+    override fun handleOnPositiveButtonClick(dialog: DialogInterface, which: Int) {
+        setResult(KEY_SELECTED_BUTTON, DialogInterface.BUTTON_POSITIVE)
+    }
+
+    override fun handleOnNegativeButtonClick(dialog: DialogInterface, which: Int) {
         // 処理なし
     }
 
-    @Override
-    protected boolean isCancelableOtherThanPressingButton() {
-        return true;
-    }
-
-    @Override
-    protected void handleOnCancel(@NonNull DialogInterface dialog) {
+    override fun handleOnCancel(dialog: DialogInterface) {
         // 処理なし
     }
 
-    @Override
-    protected void handleOnDismiss() {
+    override fun handleOnDismiss() {
         // 処理なし
     }
 }
