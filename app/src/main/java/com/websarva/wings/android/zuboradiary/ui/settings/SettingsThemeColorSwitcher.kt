00d9c1f6551de59b7d2921e326936505ce6ceed3
Takuya USAMI <tusami@gmail.com>
2025-01-31 14:57:10 +0900

    Java -> Kotlin 変換 (SettingsThemeColorSwitcher)

--- app/src/main/java/com/websarva/wings/android/zuboradiary/ui/settings/SettingsThemeColorSwitcher.kt
+++ app/src/main/java/com/websarva/wings/android/zuboradiary/ui/settings/SettingsThemeColorSwitcher.kt
@@ -1,33 +1,21 @@
-package com.websarva.wings.android.zuboradiary.ui.settings;
+package com.websarva.wings.android.zuboradiary.ui.settings
 
-import android.content.Context;
-import android.widget.TextView;
+import android.content.Context
+import android.widget.TextView
+import com.websarva.wings.android.zuboradiary.data.preferences.ThemeColor
+import com.websarva.wings.android.zuboradiary.ui.ThemeColorSwitcher
 
-import com.websarva.wings.android.zuboradiary.data.preferences.ThemeColor;
-import com.websarva.wings.android.zuboradiary.ui.ThemeColorSwitcher;
+internal class SettingsThemeColorSwitcher(context: Context, themeColor: ThemeColor)
+    : ThemeColorSwitcher(context, themeColor) {
 
-import java.util.List;
-import java.util.Objects;
-
-class SettingsThemeColorSwitcher extends ThemeColorSwitcher {
-    SettingsThemeColorSwitcher(Context context, ThemeColor themeColor) {
-        super(context, themeColor);
+    fun switchSettingItemSectionColor(textViewList: List<TextView>) {
+        val color = themeColor.getSecondaryContainerColor(resources)
+        val onColor = themeColor.getOnSecondaryContainerColor(resources)
+        switchTextViewsColor(textViewList, color, onColor)
     }
 
-    void switchSettingItemSectionColor(List<TextView> textViewList) {
-        Objects.requireNonNull(textViewList);
-        textViewList.forEach(Objects::requireNonNull);
-
-        int color = getThemeColor().getSecondaryContainerColor(getResources());
-        int onColor = getThemeColor().getOnSecondaryContainerColor(getResources());
-        switchTextViewsColor(textViewList, color, onColor);
-    }
-
-    void switchSettingItemIconColor(List<TextView> textViewList) {
-        Objects.requireNonNull(textViewList);
-        textViewList.forEach(Objects::requireNonNull);
-
-        int color = getThemeColor().getOnSurfaceVariantColor(getResources());
-        switchTextViewsColorOnlyIcon(textViewList, color);
+    fun switchSettingItemIconColor(textViewList: List<TextView>) {
+        val color = themeColor.getOnSurfaceVariantColor(resources)
+        switchTextViewsColorOnlyIcon(textViewList, color)
     }
 }
