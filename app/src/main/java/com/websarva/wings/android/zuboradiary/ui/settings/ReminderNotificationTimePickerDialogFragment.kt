0a5fdd3de6f612d0a2d0dbc6f946fcc9dafd3b8c
Takuya USAMI <tusami@gmail.com>
2025-01-31 14:48:31 +0900

    Java -> Kotlin 変換 (ReminderNotificationTimePickerDialogFragment)

--- app/src/main/java/com/websarva/wings/android/zuboradiary/ui/settings/ReminderNotificationTimePickerDialogFragment.kt
+++ app/src/main/java/com/websarva/wings/android/zuboradiary/ui/settings/ReminderNotificationTimePickerDialogFragment.kt
@@ -1,78 +1,71 @@
-package com.websarva.wings.android.zuboradiary.ui.settings;
+package com.websarva.wings.android.zuboradiary.ui.settings
 
-import android.content.DialogInterface;
-import android.view.View;
-import android.widget.NumberPicker;
-
-import androidx.annotation.NonNull;
-
-import com.websarva.wings.android.zuboradiary.databinding.DialogFragmentNumberPickersBinding;
-import com.websarva.wings.android.zuboradiary.ui.BaseNumberPickersBottomSheetDialogFragment;
-
-import java.time.LocalTime;
-import java.util.Locale;
+import android.content.DialogInterface
+import android.view.View
+import android.widget.NumberPicker
+import com.websarva.wings.android.zuboradiary.databinding.DialogFragmentNumberPickersBinding
+import com.websarva.wings.android.zuboradiary.ui.BaseNumberPickersBottomSheetDialogFragment
+import java.time.LocalTime
+import java.util.Locale
 
 // TODO:MaterialTimePickerに置き換える？
-public class ReminderNotificationTimePickerDialogFragment extends BaseNumberPickersBottomSheetDialogFragment {
+class ReminderNotificationTimePickerDialogFragment : BaseNumberPickersBottomSheetDialogFragment() {
 
-    private static final String fromClassName = "From" + ReminderNotificationTimePickerDialogFragment.class.getName();
-    static final String KEY_SELECTED_BUTTON = "SelectedButton" + fromClassName;
-    static final String KEY_SELECTED_TIME = "SelectedTIME" + fromClassName;
-
-    @Override
-    public boolean isCancelableOtherThanPressingButton() {
-        return true;
+    companion object {
+        private val fromClassName =
+            "From" + ReminderNotificationTimePickerDialogFragment::class.java.name
+        @JvmField
+        val KEY_SELECTED_BUTTON: String = "SelectedButton$fromClassName"
+        @JvmField
+        val KEY_SELECTED_TIME: String = "SelectedTIME$fromClassName"
     }
 
-    @Override
-    protected void handleOnPositiveButtonClick(@NonNull View v) {
-        setResultSelectedYearMonth();
+    override val isCancelableOtherThanPressingButton: Boolean
+        get() = true
+
+    override fun handleOnPositiveButtonClick(v: View) {
+        setResultSelectedYearMonth()
     }
 
-    private void setResultSelectedYearMonth() {
-        setResult(KEY_SELECTED_BUTTON, DialogInterface.BUTTON_POSITIVE);
+    private fun setResultSelectedYearMonth() {
+        setResult(KEY_SELECTED_BUTTON, DialogInterface.BUTTON_POSITIVE)
 
-        int selectedHourValue = binding.numberPickerFirst.getValue();
-        int selectedMinuteValue = binding.numberPickerSecond.getValue();
-        LocalTime selectedTime = LocalTime.of(selectedHourValue, selectedMinuteValue);
-        setResult(KEY_SELECTED_TIME, selectedTime);
+        val selectedHourValue = binding.numberPickerFirst.value
+        val selectedMinuteValue = binding.numberPickerSecond.value
+        val selectedTime = LocalTime.of(selectedHourValue, selectedMinuteValue)
+        setResult(KEY_SELECTED_TIME, selectedTime)
     }
 
-    @Override
-    protected void handleOnNegativeButtonClick(@NonNull View v) {
-        setResult(KEY_SELECTED_BUTTON, DialogInterface.BUTTON_NEGATIVE);
+    override fun handleOnNegativeButtonClick(v: View) {
+        setResult(KEY_SELECTED_BUTTON, DialogInterface.BUTTON_NEGATIVE)
     }
 
-    @Override
-    protected void handleOnCancel(@NonNull DialogInterface dialog) {
-        setResult(KEY_SELECTED_BUTTON, DialogInterface.BUTTON_NEGATIVE);
+    override fun handleOnCancel(dialog: DialogInterface) {
+        setResult(KEY_SELECTED_BUTTON, DialogInterface.BUTTON_NEGATIVE)
     }
 
-    @Override
-    protected void handleOnDismiss() {
+    override fun handleOnDismiss() {
         // 処理なし
     }
 
-    @Override
-    protected void setUpNumberPickers(DialogFragmentNumberPickersBinding binding) {
-        LocalTime localTime = LocalTime.now();
-        this.binding.numberPickerFirst.setMaxValue(23);
-        this.binding.numberPickerFirst.setMinValue(0);
-        this.binding.numberPickerFirst.setFormatter(new valueFormatter());
-        this.binding.numberPickerFirst.setWrapSelectorWheel(false);
-        this.binding.numberPickerFirst.setValue(localTime.getHour());
-        this.binding.numberPickerSecond.setMaxValue(59);
-        this.binding.numberPickerSecond.setMinValue(0);
-        this.binding.numberPickerSecond.setFormatter(new valueFormatter());
-        this.binding.numberPickerSecond.setWrapSelectorWheel(false);
-        this.binding.numberPickerSecond.setValue(localTime.getMinute());
-        this.binding.numberPickerThird.setVisibility(View.GONE);
+    override fun setUpNumberPickers(binding: DialogFragmentNumberPickersBinding) {
+        val localTime = LocalTime.now()
+        binding.numberPickerFirst.maxValue = 23
+        binding.numberPickerFirst.minValue = 0
+        binding.numberPickerFirst.setFormatter(ValueFormatter())
+        binding.numberPickerFirst.wrapSelectorWheel = false
+        binding.numberPickerFirst.value = localTime.hour
+        binding.numberPickerSecond.maxValue = 59
+        binding.numberPickerSecond.minValue = 0
+        binding.numberPickerSecond.setFormatter(ValueFormatter())
+        binding.numberPickerSecond.wrapSelectorWheel = false
+        binding.numberPickerSecond.value = localTime.minute
+        binding.numberPickerThird.visibility = View.GONE
     }
 
-    private static class valueFormatter implements NumberPicker.Formatter {
-        @Override
-        public String format(int value) {
-            return String.format(Locale.getDefault(),"%02d", value);
+    private class ValueFormatter : NumberPicker.Formatter {
+        override fun format(value: Int): String {
+            return String.format(Locale.getDefault(), "%02d", value)
         }
     }
 }
