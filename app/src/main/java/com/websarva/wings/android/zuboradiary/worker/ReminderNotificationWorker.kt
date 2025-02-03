fbe49c48a209c78027dada6073810c98dfd66bbe
Takuya USAMI <tusami@gmail.com>
2025-02-03 10:18:46 +0900

    Java -> Kotlin 変換 (ReminderNotificationWorker)

--- app/src/main/java/com/websarva/wings/android/zuboradiary/worker/ReminderNotificationWorker.kt
+++ app/src/main/java/com/websarva/wings/android/zuboradiary/worker/ReminderNotificationWorker.kt
@@ -1,143 +1,123 @@
-package com.websarva.wings.android.zuboradiary.worker;
+package com.websarva.wings.android.zuboradiary.worker
 
-import android.Manifest;
-import android.app.Notification;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Context;
-import android.content.pm.PackageManager;
-import android.os.Build;
-import android.util.Log;
-
-import androidx.annotation.NonNull;
-import androidx.core.app.ActivityCompat;
-import androidx.core.app.NotificationCompat;
-import androidx.core.app.NotificationManagerCompat;
-import androidx.hilt.work.HiltWorker;
-import androidx.navigation.NavDeepLinkBuilder;
-import androidx.work.Worker;
-import androidx.work.WorkerParameters;
-
-import com.google.common.util.concurrent.ListenableFuture;
-import com.websarva.wings.android.zuboradiary.CustomApplication;
-import com.websarva.wings.android.zuboradiary.R;
-import com.websarva.wings.android.zuboradiary.data.database.DiaryRepository;
-
-import java.time.LocalDate;
-import java.util.Objects;
-
-import dagger.assisted.Assisted;
-import dagger.assisted.AssistedInject;
+import android.Manifest
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
+import android.util.Log
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.hilt.work.HiltWorker
+import androidx.navigation.NavDeepLinkBuilder
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+import com.websarva.wings.android.zuboradiary.CustomApplication
+import com.websarva.wings.android.zuboradiary.R
+import com.websarva.wings.android.zuboradiary.data.database.DiaryRepository
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedInject
+import java.time.LocalDate
 
 @HiltWorker
-public class ReminderNotificationWorker extends Worker {
+class ReminderNotificationWorker @AssistedInject constructor(
+    @Assisted context: Context,
+    @Assisted workerParams: WorkerParameters,
+    private val diaryRepository: DiaryRepository
+) : Worker(context, workerParams) {
+    private val channelId: String = context.getString(R.string.reminder_notification_worker_channel_id)
+    private val channelName: String = context.getString(R.string.reminder_notification_worker_channel_name)
+    private val channelDescription: String = context.getString(R.string.reminder_notification_worker_channel_description)
+    private val channelTitle: String = context.getString(R.string.reminder_notification_worker_content_title)
+    private val channelText: String = context.getString(R.string.reminder_notification_worker_content_text)
+    private val notifyId: Int = 100
 
-    private final String CHANNEL_ID;
-    private final String CHANNEL_NAME;
-    private final String CHANNEL_DESCRIPTION;
-    private final String CONTENT_TITLE;
-    private final String CONTENT_TEXT;
-    private final int NOTIFY_ID;
-    private final DiaryRepository diaryRepository;
-
-    @AssistedInject
-    public ReminderNotificationWorker(
-            @Assisted Context context,
-            @Assisted WorkerParameters workerParams,
-            DiaryRepository diaryRepository) {
-        super(context, workerParams);
-        Objects.requireNonNull(diaryRepository);
-
-        CHANNEL_ID = context.getString(R.string.reminder_notification_worker_channel_id);
-        CHANNEL_NAME = context.getString(R.string.reminder_notification_worker_channel_name);
-        CHANNEL_DESCRIPTION = context.getString(R.string.reminder_notification_worker_channel_description);
-        CONTENT_TITLE = context.getString(R.string.reminder_notification_worker_content_title);
-        CONTENT_TEXT = context.getString(R.string.reminder_notification_worker_content_text);
-        NOTIFY_ID = 100;
-
-        this.diaryRepository = diaryRepository;
-        prepareNotificationManager();
+    init {
+        prepareNotificationManager()
     }
 
-    @NonNull
-    @Override
-    public Result doWork() {
-        CustomApplication application = (CustomApplication) getApplicationContext();
-        boolean isAppInForeground = application.isAppInForeground();
+    override fun doWork(): Result {
+        val application = applicationContext as CustomApplication
+        val isAppInForeground = application.isAppInForeground
 
-        if (isAppInForeground) return Result.success();
-        boolean hasWriteTodayDiary;
+        if (isAppInForeground) return Result.success()
+        val hasWriteTodayDiary: Boolean
         try {
-            hasWriteTodayDiary = existsSavedTodayDiary();
-        } catch (Exception e) {
-            Log.d("Exception", "本日付日記保存済み確認失敗", e);
-            return Result.failure();
+            hasWriteTodayDiary = existsSavedTodayDiary()
+        } catch (e: Exception) {
+            Log.d("Exception", "本日付日記保存済み確認失敗", e)
+            return Result.failure()
         }
-        if (hasWriteTodayDiary) return Result.success();
-        return showHeadsUpNotification();
+        if (hasWriteTodayDiary) return Result.success()
+        return showHeadsUpNotification()
     }
 
-    private void prepareNotificationManager() {
-        NotificationManager notificationManager =
-                (NotificationManager)
-                        getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
+    private fun prepareNotificationManager() {
+        val notificationManager =
+            applicationContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
         // MEMO:NotificationChannelはSdk26以降の機能
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-            NotificationChannel channel =
-                    new NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_HIGH);
-            channel.setDescription(CHANNEL_DESCRIPTION);
-            channel.setLockscreenVisibility(Notification.VISIBILITY_PUBLIC);
-            channel.enableVibration(true);
-            channel.setShowBadge(true);
-            notificationManager.createNotificationChannel(channel);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel =
+                NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_HIGH)
+                    .apply{
+                        description = channelDescription
+                        lockscreenVisibility = Notification.VISIBILITY_PUBLIC
+                        enableVibration(true)
+                        setShowBadge(true)
+                    }
+            notificationManager.createNotificationChannel(channel)
         }
     }
 
-    private boolean existsSavedTodayDiary() throws Exception{
-        ListenableFuture<Boolean> listenableFuture = diaryRepository.existsDiary(LocalDate.now());
-        Boolean result = listenableFuture.get();
-        if (result == null) return false;
-        Log.d("NotificationWorker", "hasWriteTodayDiary():" + result);
-        return result;
+    @Throws(Exception::class)
+    private fun existsSavedTodayDiary(): Boolean {
+        val listenableFuture = diaryRepository.existsDiary(LocalDate.now())
+        val result = listenableFuture.get() ?: return false
+        Log.d("NotificationWorker", "hasWriteTodayDiary():$result")
+        return result
     }
 
-    private Result showHeadsUpNotification() {
-        boolean isPermission;
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            isPermission =
-                    ActivityCompat.checkSelfPermission(
-                            getApplicationContext(), Manifest.permission.POST_NOTIFICATIONS)
-                            == PackageManager.PERMISSION_GRANTED;
-        } else {
-            isPermission = true;
-        }
-        Log.d("NotificationWorker", "showHeadsUpNotification()_NotificationIsPermission:" + isPermission);
+    private fun showHeadsUpNotification(): Result {
+        val isPermission =
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                ActivityCompat.checkSelfPermission(
+                    applicationContext, Manifest.permission.POST_NOTIFICATIONS
+                ) == PackageManager.PERMISSION_GRANTED
+            } else {
+                true
+            }
+        Log.d(
+            "NotificationWorker",
+            "showHeadsUpNotification()_NotificationIsPermission:$isPermission"
+        )
 
-        if (!isPermission) return Result.failure();
+        if (!isPermission) return Result.failure()
 
-        PendingIntent pendingIntent =
-                new NavDeepLinkBuilder(getApplicationContext())
-                        .setGraph(R.navigation.mobile_navigation)
-                        .setDestination(R.id.navigation_diary_list_fragment)
-                        .createPendingIntent();
+        val pendingIntent =
+            NavDeepLinkBuilder(applicationContext)
+                .setGraph(R.navigation.mobile_navigation)
+                .setDestination(R.id.navigation_diary_list_fragment)
+                .createPendingIntent()
 
-        NotificationCompat.Builder builder =
-                new NotificationCompat.Builder(getApplicationContext(), CHANNEL_ID);
-        Notification notification =
-                builder.setSmallIcon(R.drawable.ic_notifications_24px)
-                        .setContentTitle(CONTENT_TITLE)
-                        .setContentText(CONTENT_TEXT)
-                        .setPriority(NotificationCompat.PRIORITY_MAX)
-                        .setDefaults(Notification.DEFAULT_ALL)
-                        .setContentIntent(pendingIntent)
-                        .setAutoCancel(true)
-                        .build();
+        val builder =
+            NotificationCompat.Builder(applicationContext, channelId)
+        val notification =
+            builder.setSmallIcon(R.drawable.ic_notifications_24px)
+                .setContentTitle(channelTitle)
+                .setContentText(channelText)
+                .setPriority(NotificationCompat.PRIORITY_MAX)
+                .setDefaults(Notification.DEFAULT_ALL)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true)
+                .build()
 
-        NotificationManagerCompat notificationManager =
-                NotificationManagerCompat.from(getApplicationContext());
-        notificationManager.notify(NOTIFY_ID, notification);
-        return Result.success();
+        val notificationManager =
+            NotificationManagerCompat.from(applicationContext)
+        notificationManager.notify(notifyId, notification)
+        return Result.success()
     }
 }
