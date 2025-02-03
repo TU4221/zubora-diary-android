eb393079b0035ea959ba0bc8c5ccba57e73806b2
Takuya USAMI <tusami@gmail.com>
2025-02-03 10:50:31 +0900

    Java -> Kotlin 変換 (DiaryDayListBaseItem)

--- app/src/main/java/com/websarva/wings/android/zuboradiary/ui/list/DiaryDayListBaseItem.kt
+++ app/src/main/java/com/websarva/wings/android/zuboradiary/ui/list/DiaryDayListBaseItem.kt
@@ -1,25 +1,9 @@
-package com.websarva.wings.android.zuboradiary.ui.list;
+package com.websarva.wings.android.zuboradiary.ui.list
 
-import androidx.annotation.NonNull;
+import com.websarva.wings.android.zuboradiary.data.database.DiaryListBaseItem
+import java.time.LocalDate
+import java.util.Objects
 
-import com.websarva.wings.android.zuboradiary.data.database.DiaryListBaseItem;
-
-import java.time.LocalDate;
-import java.util.Objects;
-
-public abstract class DiaryDayListBaseItem {
-    private final LocalDate date;
-
-    public DiaryDayListBaseItem(DiaryListBaseItem listItem) {
-        Objects.requireNonNull(listItem);
-
-        String dateString = listItem.getDate();
-        this.date = LocalDate.parse(dateString);
-    }
-
-    @NonNull
-    public LocalDate getDate() {
-        return date;
-    }
-
+abstract class DiaryDayListBaseItem(listItem: DiaryListBaseItem) {
+    val date: LocalDate = listItem.date.let { LocalDate.parse(it) }
 }
