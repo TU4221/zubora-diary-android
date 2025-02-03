6ba68f52315f7632f79082f7a7b419f894efe377
Takuya USAMI <tusami@gmail.com>
2025-02-03 16:01:17 +0900

    Java -> Kotlin 変換 (WordSearchResultYearMonthList)

--- app/src/main/java/com/websarva/wings/android/zuboradiary/ui/list/wordsearch/WordSearchResultYearMonthList.kt
+++ app/src/main/java/com/websarva/wings/android/zuboradiary/ui/list/wordsearch/WordSearchResultYearMonthList.kt
@@ -1,166 +1,173 @@
-package com.websarva.wings.android.zuboradiary.ui.list.wordsearch;
+package com.websarva.wings.android.zuboradiary.ui.list.wordsearch
 
-import androidx.annotation.NonNull;
+import com.websarva.wings.android.zuboradiary.ui.list.DiaryYearMonthListBaseAdapter
+import com.websarva.wings.android.zuboradiary.ui.list.DiaryYearMonthListBaseItem
+import java.time.YearMonth
 
-import com.websarva.wings.android.zuboradiary.ui.list.DiaryYearMonthListBaseAdapter.ViewType;
-import com.websarva.wings.android.zuboradiary.ui.list.DiaryYearMonthListBaseItem;
+class WordSearchResultYearMonthList {
 
-import java.time.LocalDate;
-import java.time.YearMonth;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Objects;
+    val wordSearchResultYearMonthListItemList: List<WordSearchResultYearMonthListItem>
 
-public class WordSearchResultYearMonthList {
+    internal constructor(
+        wordSearchResultDayList: WordSearchResultDayList,
+        needsNoDiaryMessage: Boolean
+    ) {
+        require(wordSearchResultDayList.wordSearchResultDayListItemList.isNotEmpty())
 
-    private final List<WordSearchResultYearMonthListItem> wordSearchResultYearMonthListItemList;
-
-    WordSearchResultYearMonthList(WordSearchResultDayList wordSearchResultDayList, boolean needsNoDiaryMessage) {
-        Objects.requireNonNull(wordSearchResultDayList);
-        if (wordSearchResultDayList.getWordSearchResultDayListItemList().isEmpty()) throw new IllegalArgumentException();
-
-        List<WordSearchResultYearMonthListItem> WordSearchResultYearMonthListItemList = createWordSearchResultYearMonthListItem(wordSearchResultDayList);
-        addLastItem(WordSearchResultYearMonthListItemList, needsNoDiaryMessage);
-        this.wordSearchResultYearMonthListItemList = Collections.unmodifiableList(WordSearchResultYearMonthListItemList);
+        val itemList =
+            createWordSearchResultYearMonthListItem(wordSearchResultDayList)
+        this.wordSearchResultYearMonthListItemList = addLastItem(itemList, needsNoDiaryMessage)
     }
 
-    WordSearchResultYearMonthList(List<WordSearchResultYearMonthListItem> itemList, boolean needsNoDiaryMessage) {
-        Objects.requireNonNull(itemList);
-        if (itemList.isEmpty()) throw new IllegalArgumentException();
-        itemList.stream().forEach(Objects::requireNonNull);
+    constructor(
+        itemList: List<WordSearchResultYearMonthListItem>,
+        needsNoDiaryMessage: Boolean
+    ) {
+        require(itemList.isNotEmpty())
 
-        List<WordSearchResultYearMonthListItem> _itemList = new ArrayList<>(itemList);
-        addLastItem(_itemList, needsNoDiaryMessage);
-        this.wordSearchResultYearMonthListItemList = Collections.unmodifiableList(_itemList);
+        this.wordSearchResultYearMonthListItemList = addLastItem(itemList, needsNoDiaryMessage)
     }
 
     /**
-     * true:日記なしメッセージのみのリスト作成<br>
+     * true:日記なしメッセージのみのリスト作成<br></br>
      * false:ProgressIndicatorのみのリスト作成
-     * */
-    WordSearchResultYearMonthList(boolean needsNoDiaryMessage) {
-        List<WordSearchResultYearMonthListItem> itemList = new ArrayList<>();
-        addLastItem(itemList, needsNoDiaryMessage);
-        this.wordSearchResultYearMonthListItemList = Collections.unmodifiableList(itemList);
+     */
+    constructor(needsNoDiaryMessage: Boolean) {
+        val emptyList: MutableList<WordSearchResultYearMonthListItem> = ArrayList()
+        this.wordSearchResultYearMonthListItemList = addLastItem(emptyList, needsNoDiaryMessage)
     }
 
-    WordSearchResultYearMonthList() {
-        List<WordSearchResultYearMonthListItem> itemList = new ArrayList<>();
-        this.wordSearchResultYearMonthListItemList = Collections.unmodifiableList(itemList);
+    constructor() {
+        this.wordSearchResultYearMonthListItemList = ArrayList()
     }
 
-    @NonNull
-    private List<WordSearchResultYearMonthListItem> createWordSearchResultYearMonthListItem(WordSearchResultDayList wordSearchResultDayList) {
-        Objects.requireNonNull(wordSearchResultDayList);
-        if (wordSearchResultDayList.getWordSearchResultDayListItemList().isEmpty()) throw new IllegalArgumentException();
+    private fun createWordSearchResultYearMonthListItem(
+        wordSearchResultDayList: WordSearchResultDayList
+    ): List<WordSearchResultYearMonthListItem> {
+        require(wordSearchResultDayList.wordSearchResultDayListItemList.isNotEmpty())
 
-        List<WordSearchResultDayListItem> sortingDayItemList= new ArrayList<>();
-        List<WordSearchResultYearMonthListItem> resultYearMonthListItemList = new ArrayList<>();
-        WordSearchResultYearMonthListItem resultYearMonthListItem;
-        YearMonth sortingYearMonth = null;
+        var sortingDayItemList: MutableList<WordSearchResultDayListItem> = ArrayList()
+        val resultYearMonthListItemList: MutableList<WordSearchResultYearMonthListItem> = ArrayList()
+        var resultYearMonthListItem: WordSearchResultYearMonthListItem
+        var sortingYearMonth: YearMonth? = null
 
-        List<WordSearchResultDayListItem> resultDayListItemList = wordSearchResultDayList.getWordSearchResultDayListItemList();
-        for (WordSearchResultDayListItem day: resultDayListItemList) {
-            LocalDate date = day.getDate();
-            YearMonth yearMonth = YearMonth.of(date.getYear(), date.getMonth());
+        // DayListItemを対象YearMonthListItem毎に仕分け
+        val resultDayListItemList = wordSearchResultDayList.wordSearchResultDayListItemList
+        for (day in resultDayListItemList) {
+            val date = day.date
+            val yearMonth = YearMonth.of(date.year, date.month)
 
-            if (sortingYearMonth != null && !yearMonth.equals(sortingYearMonth)) {
-                WordSearchResultDayList sortedWordSearchResultDayList = new WordSearchResultDayList(sortingDayItemList);
+            if (sortingYearMonth != null && yearMonth != sortingYearMonth) {
+                val sortedWordSearchResultDayList = WordSearchResultDayList(sortingDayItemList)
                 resultYearMonthListItem =
-                        new WordSearchResultYearMonthListItem(sortingYearMonth, sortedWordSearchResultDayList);
-                resultYearMonthListItemList.add(resultYearMonthListItem);
-                sortingDayItemList= new ArrayList<>();
+                    WordSearchResultYearMonthListItem(
+                        sortingYearMonth,
+                        sortedWordSearchResultDayList
+                    )
+                resultYearMonthListItemList.add(resultYearMonthListItem)
+                sortingDayItemList = ArrayList()
             }
-            sortingDayItemList.add(day);
-            sortingYearMonth = yearMonth;
+            sortingDayItemList.add(day)
+            sortingYearMonth = yearMonth
         }
 
-        WordSearchResultDayList sortedWordSearchResultDayList = new WordSearchResultDayList(sortingDayItemList);
+        // 最後尾YearMonthListItemの追加処理
+        val sortedWordSearchResultDayList = WordSearchResultDayList(sortingDayItemList)
         resultYearMonthListItem =
-                new WordSearchResultYearMonthListItem(sortingYearMonth, sortedWordSearchResultDayList);
-        resultYearMonthListItemList.add(resultYearMonthListItem);
-        return resultYearMonthListItemList;
+            WordSearchResultYearMonthListItem(sortingYearMonth, sortedWordSearchResultDayList)
+        resultYearMonthListItemList.add(resultYearMonthListItem)
+
+        return resultYearMonthListItemList
     }
 
-    private void addLastItem(List<WordSearchResultYearMonthListItem> itemList, boolean needsNoDiaryMessage) {
-        Objects.requireNonNull(itemList);
-        itemList.stream().forEach(Objects::requireNonNull);
+    private fun addLastItem(
+        itemList: List<WordSearchResultYearMonthListItem>,
+        needsNoDiaryMessage: Boolean
+    ): List<WordSearchResultYearMonthListItem> {
+        val mutableItemList = itemList.toMutableList()
+        mutableItemList.removeIf(DiaryYearMonthListBaseItem::isNotDiaryViewType)
 
-        itemList.removeIf(DiaryYearMonthListBaseItem::isNotDiaryViewType);
-        if (needsNoDiaryMessage) {
-            addLastItemNoDiaryMessage(itemList);
+        return if (needsNoDiaryMessage) {
+            addLastItemNoDiaryMessage(mutableItemList)
         } else {
-            addLastItemProgressIndicator(itemList);
+            addLastItemProgressIndicator(mutableItemList)
         }
     }
 
-    private void addLastItemProgressIndicator(List<WordSearchResultYearMonthListItem> itemList) {
-        Objects.requireNonNull(itemList);
-        itemList.stream().forEach(Objects::requireNonNull);
+    private fun addLastItemProgressIndicator(
+        itemList: List<WordSearchResultYearMonthListItem>
+    ): List<WordSearchResultYearMonthListItem> {
 
-        itemList.add(new WordSearchResultYearMonthListItem(ViewType.PROGRESS_INDICATOR));
+        return itemList +
+                WordSearchResultYearMonthListItem(
+                    DiaryYearMonthListBaseAdapter.ViewType.PROGRESS_INDICATOR
+                )
     }
 
-    private void addLastItemNoDiaryMessage(List<WordSearchResultYearMonthListItem> itemList) {
-        Objects.requireNonNull(itemList);
-        itemList.stream().forEach(Objects::requireNonNull);
-
-        itemList.add(new WordSearchResultYearMonthListItem(ViewType.NO_DIARY_MESSAGE));
+    private fun addLastItemNoDiaryMessage(
+        itemList: List<WordSearchResultYearMonthListItem>
+    ): List<WordSearchResultYearMonthListItem> {
+        return itemList +
+                WordSearchResultYearMonthListItem(
+                    DiaryYearMonthListBaseAdapter.ViewType.NO_DIARY_MESSAGE
+                )
     }
 
-    public int countDiaries() {
-        int count = 0;
-        for (WordSearchResultYearMonthListItem item: wordSearchResultYearMonthListItemList) {
-            if (item.getViewType().equals(ViewType.DIARY)) {
-                count += item.getWordSearchResultDayList().countDiaries();
+    fun countDiaries(): Int {
+        var count = 0
+        for (item in wordSearchResultYearMonthListItemList) {
+            if (item.viewType == DiaryYearMonthListBaseAdapter.ViewType.DIARY) {
+                count += item.wordSearchResultDayList.countDiaries()
             }
         }
-        return count;
+        return count
     }
 
-    WordSearchResultYearMonthList combineDiaryLists(
-            WordSearchResultYearMonthList additionList, boolean needsNoDiaryMessage) {
-        Objects.requireNonNull(additionList);
-        if (additionList.getWordSearchResultYearMonthListItemList().isEmpty()) throw new IllegalArgumentException();
+    fun combineDiaryLists(
+        additionList: WordSearchResultYearMonthList, needsNoDiaryMessage: Boolean
+    ): WordSearchResultYearMonthList {
+        require(additionList.wordSearchResultYearMonthListItemList.isNotEmpty())
 
-        List<WordSearchResultYearMonthListItem> originalItemList = new ArrayList<>(this.wordSearchResultYearMonthListItemList);
-        List<WordSearchResultYearMonthListItem> additionItemList = new ArrayList<>(additionList.wordSearchResultYearMonthListItemList);
+        val originalItemList: MutableList<WordSearchResultYearMonthListItem> =
+            wordSearchResultYearMonthListItemList.toMutableList()
+        val additionItemList: MutableList<WordSearchResultYearMonthListItem> =
+            additionList.wordSearchResultYearMonthListItemList.toMutableList()
 
         // List最終アイテム(日記以外
-        originalItemList.removeIf(DiaryYearMonthListBaseItem::isNotDiaryViewType);
-        additionItemList.removeIf(DiaryYearMonthListBaseItem::isNotDiaryViewType);
+        originalItemList.removeIf(DiaryYearMonthListBaseItem::isNotDiaryViewType)
+        additionItemList.removeIf(DiaryYearMonthListBaseItem::isNotDiaryViewType)
 
         // 元リスト最終アイテムの年月取得
-        int originalListLastItemPosition = originalItemList.size() - 1;
-        WordSearchResultYearMonthListItem originalListLastItem = originalItemList.get(originalListLastItemPosition);
-        YearMonth originalListLastItemYearMonth = originalListLastItem.getYearMonth();
+        val originalListLastItemPosition = originalItemList.size - 1
+        val originalListLastItem = originalItemList[originalListLastItemPosition]
+        val originalListLastItemYearMonth = originalListLastItem.yearMonth
 
         // 追加リスト先頭アイテムの年月取得
-        WordSearchResultYearMonthListItem additionListFirstItem = additionList.getWordSearchResultYearMonthListItemList().get(0);
-        YearMonth additionListFirstItemYearMonth = additionListFirstItem.getYearMonth();
+        val additionListFirstItem = additionList.wordSearchResultYearMonthListItemList[0]
+        val additionListFirstItemYearMonth = additionListFirstItem.yearMonth
 
         // 元リストに追加リストの年月が含まれていたらアイテムを足し込む
-        if (originalListLastItemYearMonth.equals(additionListFirstItemYearMonth)) {
-            WordSearchResultDayList originalLastWordSearchResultDayList =
-                    originalItemList.get(originalListLastItemPosition).getWordSearchResultDayList();
-            WordSearchResultDayList additionWordSearchResultDayList = additionListFirstItem.getWordSearchResultDayList();
-            WordSearchResultDayList combinedWordSearchResultDayList =
-                    originalLastWordSearchResultDayList.combineDiaryDayLists(additionWordSearchResultDayList);
-            WordSearchResultYearMonthListItem combinedResultYearMonthListItem
-                    = new WordSearchResultYearMonthListItem(originalListLastItemYearMonth, combinedWordSearchResultDayList);
-            originalItemList.remove(originalListLastItemPosition);
-            originalItemList.add(combinedResultYearMonthListItem);
-            additionItemList.remove(0);
+        if (originalListLastItemYearMonth == additionListFirstItemYearMonth) {
+            val originalLastWordSearchResultDayList =
+                originalItemList[originalListLastItemPosition].wordSearchResultDayList
+            val additionWordSearchResultDayList = additionListFirstItem.wordSearchResultDayList
+            val combinedWordSearchResultDayList =
+                originalLastWordSearchResultDayList.combineDiaryDayLists(
+                    additionWordSearchResultDayList
+                )
+            val combinedResultYearMonthListItem = WordSearchResultYearMonthListItem(
+                originalListLastItemYearMonth,
+                combinedWordSearchResultDayList
+            )
+            originalItemList.removeAt(originalListLastItemPosition)
+            originalItemList.add(combinedResultYearMonthListItem)
+            additionItemList.removeAt(0)
         }
 
-        List<WordSearchResultYearMonthListItem> resultItemList = new ArrayList<>(originalItemList);
-        resultItemList.addAll(additionItemList);
+        val resultItemList: MutableList<WordSearchResultYearMonthListItem> =
+            originalItemList.toMutableList()
+        resultItemList.addAll(additionItemList)
 
-        return new WordSearchResultYearMonthList(resultItemList, needsNoDiaryMessage);
-    }
-
-    public List<WordSearchResultYearMonthListItem> getWordSearchResultYearMonthListItemList() {
-        return wordSearchResultYearMonthListItemList;
+        return WordSearchResultYearMonthList(resultItemList, needsNoDiaryMessage)
     }
 }
