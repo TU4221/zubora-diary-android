50bcfa534696100f3c5333c05b393ae58bb9ab65
Takuya USAMI <tusami@gmail.com>
2025-02-04 16:48:21 +0900

    Java -> Kotlin 変換 (WorkerRepository)

--- app/src/main/java/com/websarva/wings/android/zuboradiary/data/worker/WorkerRepository.kt
+++ app/src/main/java/com/websarva/wings/android/zuboradiary/data/worker/WorkerRepository.kt
@@ -1,61 +1,52 @@
-package com.websarva.wings.android.zuboradiary.data.worker;
+package com.websarva.wings.android.zuboradiary.data.worker
 
-import android.util.Log;
+import android.util.Log
+import androidx.work.ExistingPeriodicWorkPolicy
+import androidx.work.PeriodicWorkRequest
+import androidx.work.WorkManager
+import com.websarva.wings.android.zuboradiary.worker.ReminderNotificationWorker
+import java.time.LocalTime
+import java.time.temporal.ChronoUnit
+import java.util.concurrent.TimeUnit
+import javax.inject.Inject
 
-import androidx.work.ExistingPeriodicWorkPolicy;
-import androidx.work.PeriodicWorkRequest;
-import androidx.work.WorkManager;
+class WorkerRepository @Inject constructor(private val workManager: WorkManager) {
 
-import com.websarva.wings.android.zuboradiary.worker.ReminderNotificationWorker;
+    private val workTagReminderNotification = "ReminderNotification"
+    private val uniqueWorkNameReminderNotification = workTagReminderNotification
 
-import java.time.LocalTime;
-import java.time.temporal.ChronoUnit;
-import java.util.Objects;
-import java.util.concurrent.TimeUnit;
+    fun registerReminderNotificationWorker(settingTime: LocalTime) {
+        cancelReminderNotificationWorker()
 
-import javax.inject.Inject;
-
-public class WorkerRepository {
-
-    private final WorkManager workManager;
-    private final String WORK_TAG_REMINDER_NOTIFICATION = "ReminderNotification";
-    private final String UNIQUE_WORK_NAME_REMINDER_NOTIFICATION = WORK_TAG_REMINDER_NOTIFICATION;
-
-    @Inject
-    public WorkerRepository(WorkManager workManager) {
-        Objects.requireNonNull(workManager);
-
-        this.workManager = workManager;
-    }
-
-    public void registerReminderNotificationWorker(LocalTime settingTime) {
-        Objects.requireNonNull(settingTime);
-
-        cancelReminderNotificationWorker();
-
-        LocalTime nowTime = LocalTime.now();
-        long initialDelaySeconds = calculationBetweenSeconds(nowTime, settingTime);
-        Log.d("NotificationWorker", "initialDelaySeconds:" + initialDelaySeconds);
-        PeriodicWorkRequest request =
-                new PeriodicWorkRequest
-                        .Builder(ReminderNotificationWorker.class, 1, TimeUnit.DAYS)
-                        .addTag(WORK_TAG_REMINDER_NOTIFICATION)
-                        .setInitialDelay(initialDelaySeconds, TimeUnit.SECONDS)
-                        .build();
+        val nowTime = LocalTime.now()
+        val initialDelaySeconds = calculationBetweenSeconds(nowTime, settingTime)
+        Log.d("NotificationWorker", "initialDelaySeconds:$initialDelaySeconds")
+        val request =
+            PeriodicWorkRequest
+                .Builder(ReminderNotificationWorker::class.java, 1, TimeUnit.DAYS)
+                .addTag(workTagReminderNotification)
+                .setInitialDelay(initialDelaySeconds, TimeUnit.SECONDS)
+                .build()
         workManager.enqueueUniquePeriodicWork(
-                UNIQUE_WORK_NAME_REMINDER_NOTIFICATION, ExistingPeriodicWorkPolicy.CANCEL_AND_REENQUEUE, request);
+            uniqueWorkNameReminderNotification,
+            ExistingPeriodicWorkPolicy.CANCEL_AND_REENQUEUE,
+            request
+        )
     }
 
-    private long calculationBetweenSeconds(LocalTime startTime, LocalTime endTime) {
-        long betweenSeconds = ChronoUnit.SECONDS.between(startTime, endTime);
+    private fun calculationBetweenSeconds(startTime: LocalTime, endTime: LocalTime): Long {
+        val betweenSeconds = ChronoUnit.SECONDS.between(startTime, endTime)
         if (betweenSeconds < 0) {
-            return 60/*seconds*/ * 60/*minutes*/ * 24/*hours*/ + betweenSeconds;
+            return 60 /*seconds*/ * 60 /*minutes*/ * 24 /*hours*/ + betweenSeconds
         }
-        return betweenSeconds;
+        return betweenSeconds
     }
 
-    public void cancelReminderNotificationWorker() {
-        workManager.cancelAllWorkByTag(WORK_TAG_REMINDER_NOTIFICATION);
-        workManager.cancelUniqueWork(UNIQUE_WORK_NAME_REMINDER_NOTIFICATION);
+    fun cancelReminderNotificationWorker() {
+        workManager.apply {
+            cancelAllWorkByTag(workTagReminderNotification)
+            cancelUniqueWork(uniqueWorkNameReminderNotification)
+        }
+
     }
 }
