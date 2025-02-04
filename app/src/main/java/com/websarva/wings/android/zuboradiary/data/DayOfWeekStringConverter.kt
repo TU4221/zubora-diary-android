8f206f70d848ed40bbd2420e6f8b995f57c94d4d
Takuya USAMI <tusami@gmail.com>
2025-02-04 16:27:01 +0900

    Java -> Kotlin 変換 (DayOfWeekStringConverter)

--- app/src/main/java/com/websarva/wings/android/zuboradiary/data/DayOfWeekStringConverter.kt
+++ app/src/main/java/com/websarva/wings/android/zuboradiary/data/DayOfWeekStringConverter.kt
@@ -1,89 +1,39 @@
-package com.websarva.wings.android.zuboradiary.data;
+package com.websarva.wings.android.zuboradiary.data
 
-import android.content.Context;
-
-import androidx.annotation.NonNull;
-
-import com.websarva.wings.android.zuboradiary.R;
-
-import java.time.DayOfWeek;
-import java.util.Objects;
+import android.content.Context
+import com.websarva.wings.android.zuboradiary.R
+import java.time.DayOfWeek
 
 /**
  * Enum java.time.DayOfWeeK を用途に合わせた文字列に変換するクラス。
- * */
-public class DayOfWeekStringConverter {
-    private final Context context;
+ */
+class DayOfWeekStringConverter(private val context: Context) {
 
-    public DayOfWeekStringConverter(Context context) {
-        Objects.requireNonNull(context);
-
-        this.context = context;
+    fun toCalendarStartDayOfWeek(dayOfWeek: DayOfWeek): String {
+        val resId = when (dayOfWeek) {
+            DayOfWeek.SUNDAY -> R.string.day_of_week_name_sunday
+            DayOfWeek.MONDAY -> R.string.day_of_week_name_monday
+            DayOfWeek.TUESDAY -> R.string.day_of_week_name_tuesday
+            DayOfWeek.WEDNESDAY -> R.string.day_of_week_name_wednesday
+            DayOfWeek.THURSDAY -> R.string.day_of_week_name_thursday
+            DayOfWeek.FRIDAY -> R.string.day_of_week_name_friday
+            DayOfWeek.SATURDAY -> R.string.day_of_week_name_saturday
+            else -> throw IllegalArgumentException()
+        }
+        return context.getString(resId)
     }
 
-    @NonNull
-    public String toCalendarStartDayOfWeek(DayOfWeek dayOfWeek) {
-        Objects.requireNonNull(dayOfWeek);
-
-        int resId;
-        switch (dayOfWeek) {
-            case SUNDAY:
-                resId = R.string.day_of_week_name_sunday;
-                break;
-            case MONDAY:
-                resId = R.string.day_of_week_name_monday;
-                break;
-            case TUESDAY:
-                resId = R.string.day_of_week_name_tuesday;
-                break;
-            case WEDNESDAY:
-                resId = R.string.day_of_week_name_wednesday;
-                break;
-            case THURSDAY:
-                resId = R.string.day_of_week_name_thursday;
-                break;
-            case FRIDAY:
-                resId = R.string.day_of_week_name_friday;
-                break;
-            case SATURDAY:
-                resId = R.string.day_of_week_name_saturday;
-                break;
-            default:
-                throw new IllegalStateException();
+    fun toDiaryListDayOfWeek(dayOfWeek: DayOfWeek): String {
+        val resId = when (dayOfWeek) {
+            DayOfWeek.SUNDAY -> R.string.day_of_week_short_name_sunday
+            DayOfWeek.MONDAY -> R.string.day_of_week_short_name_monday
+            DayOfWeek.TUESDAY -> R.string.day_of_week_short_name_tuesday
+            DayOfWeek.WEDNESDAY -> R.string.day_of_week_short_name_wednesday
+            DayOfWeek.THURSDAY -> R.string.day_of_week_short_name_thursday
+            DayOfWeek.FRIDAY -> R.string.day_of_week_short_name_friday
+            DayOfWeek.SATURDAY -> R.string.day_of_week_short_name_saturday
+            else -> throw IllegalArgumentException()
         }
-        return context.getString(resId);
-    }
-
-    @NonNull
-    public String toDiaryListDayOfWeek(DayOfWeek dayOfWeek) {
-        Objects.requireNonNull(dayOfWeek);
-
-        int resId;
-        switch (dayOfWeek) {
-            case SUNDAY:
-                resId = R.string.day_of_week_short_name_sunday;
-                break;
-            case MONDAY:
-                resId = R.string.day_of_week_short_name_monday;
-                break;
-            case TUESDAY:
-                resId = R.string.day_of_week_short_name_tuesday;
-                break;
-            case WEDNESDAY:
-                resId = R.string.day_of_week_short_name_wednesday;
-                break;
-            case THURSDAY:
-                resId = R.string.day_of_week_short_name_thursday;
-                break;
-            case FRIDAY:
-                resId = R.string.day_of_week_short_name_friday;
-                break;
-            case SATURDAY:
-                resId = R.string.day_of_week_short_name_saturday;
-                break;
-            default:
-                throw new IllegalStateException();
-        }
-        return context.getString(resId);
+        return context.getString(resId)
     }
 }
