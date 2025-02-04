e561b3d2e18a092a54f69d039d4678a577d5d3ff
Takuya USAMI <tusami@gmail.com>
2025-02-04 17:42:28 +0900

    Java -> Kotlin 変換 一部修正(WeatherApiService)

--- app/src/main/java/com/websarva/wings/android/zuboradiary/data/network/WeatherApiService.kt
+++ app/src/main/java/com/websarva/wings/android/zuboradiary/data/network/WeatherApiService.kt
@@ -7,11 +7,11 @@ import retrofit2.http.Query
 interface WeatherApiService {
     @GET("forecast")
     fun getWeather(
-        @Query("latitude") latitude: String?,
-        @Query("longitude") longitude: String?,
-        @Query("daily") daily: String?,
-        @Query("timezone") timezone: String?,
-        @Query("past_days") pastDays: String?,
-        @Query("forecast_days") forecastDays: String?
-    ): Call<WeatherApiResponse?>?
+        @Query("latitude") latitude: String,
+        @Query("longitude") longitude: String,
+        @Query("daily") daily: String,
+        @Query("timezone") timezone: String,
+        @Query("past_days") pastDays: String,
+        @Query("forecast_days") forecastDays: String
+    ): Call<WeatherApiResponse>
 }
