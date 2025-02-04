0d55ecd1cb21a1cebfc9ae7f4a7a3857d8517979
Takuya USAMI <tusami@gmail.com>
2025-02-04 15:59:38 +0900

    Java -> Kotlin 変換 (RepositoryModule)

--- app/src/main/java/com/websarva/wings/android/zuboradiary/di/RepositoryModule.kt
+++ app/src/main/java/com/websarva/wings/android/zuboradiary/di/RepositoryModule.kt
@@ -1,86 +1,63 @@
-package com.websarva.wings.android.zuboradiary.di;
+package com.websarva.wings.android.zuboradiary.di
 
-import androidx.annotation.NonNull;
-import androidx.work.WorkManager;
-
-import com.websarva.wings.android.zuboradiary.data.database.DiaryDAO;
-import com.websarva.wings.android.zuboradiary.data.database.DiaryDatabase;
-import com.websarva.wings.android.zuboradiary.data.database.DiaryItemTitleSelectionHistoryRepository;
-import com.websarva.wings.android.zuboradiary.data.database.DiaryRepository;
-import com.websarva.wings.android.zuboradiary.data.database.DiaryItemTitleSelectionHistoryDAO;
-import com.websarva.wings.android.zuboradiary.data.network.WeatherApiRepository;
-import com.websarva.wings.android.zuboradiary.data.network.WeatherApiService;
-import com.websarva.wings.android.zuboradiary.data.preferences.UserPreferencesRepository;
-import com.websarva.wings.android.zuboradiary.data.preferences.UserPreferences;
-import com.websarva.wings.android.zuboradiary.data.worker.WorkerRepository;
-
-import java.util.Objects;
-
-import javax.inject.Singleton;
-
-import dagger.Module;
-import dagger.Provides;
-import dagger.hilt.InstallIn;
-import dagger.hilt.components.SingletonComponent;
+import androidx.work.WorkManager
+import com.websarva.wings.android.zuboradiary.data.database.DiaryDAO
+import com.websarva.wings.android.zuboradiary.data.database.DiaryDatabase
+import com.websarva.wings.android.zuboradiary.data.database.DiaryItemTitleSelectionHistoryDAO
+import com.websarva.wings.android.zuboradiary.data.database.DiaryItemTitleSelectionHistoryRepository
+import com.websarva.wings.android.zuboradiary.data.database.DiaryRepository
+import com.websarva.wings.android.zuboradiary.data.network.WeatherApiRepository
+import com.websarva.wings.android.zuboradiary.data.network.WeatherApiService
+import com.websarva.wings.android.zuboradiary.data.preferences.UserPreferences
+import com.websarva.wings.android.zuboradiary.data.preferences.UserPreferencesRepository
+import com.websarva.wings.android.zuboradiary.data.worker.WorkerRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
-@InstallIn(SingletonComponent.class)
-public class RepositoryModule {
+@InstallIn(SingletonComponent::class)
+object RepositoryModule {
+    @JvmStatic
     @Singleton
     @Provides
-    @NonNull
-    public static DiaryRepository provideDiaryRepository(
-            DiaryDatabase diaryDatabase,
-            DiaryDAO diaryDAO,
-            DiaryItemTitleSelectionHistoryDAO diaryItemTitleSelectionHistoryDAO) {
-        Objects.requireNonNull(diaryDatabase);
-        Objects.requireNonNull(diaryDAO);
-        Objects.requireNonNull(diaryItemTitleSelectionHistoryDAO);
-
-        DiaryRepository repository =
-                new DiaryRepository(diaryDatabase, diaryDAO, diaryItemTitleSelectionHistoryDAO);
-        return Objects.requireNonNull(repository);
+    fun provideDiaryRepository(
+        diaryDatabase: DiaryDatabase,
+        diaryDAO: DiaryDAO,
+        diaryItemTitleSelectionHistoryDAO: DiaryItemTitleSelectionHistoryDAO
+    ): DiaryRepository {
+        return DiaryRepository(diaryDatabase, diaryDAO, diaryItemTitleSelectionHistoryDAO)
     }
 
+    @JvmStatic
     @Singleton
     @Provides
-    @NonNull
-    public static DiaryItemTitleSelectionHistoryRepository provideEditDiarySelectItemTitleRepository(
-            DiaryItemTitleSelectionHistoryDAO diaryItemTitleSelectionHistoryDAO) {
-        Objects.requireNonNull(diaryItemTitleSelectionHistoryDAO);
-
-        DiaryItemTitleSelectionHistoryRepository repository =
-                new DiaryItemTitleSelectionHistoryRepository(diaryItemTitleSelectionHistoryDAO);
-        return Objects.requireNonNull(repository);
+    fun provideEditDiarySelectItemTitleRepository(
+        diaryItemTitleSelectionHistoryDAO: DiaryItemTitleSelectionHistoryDAO
+    ): DiaryItemTitleSelectionHistoryRepository {
+        return DiaryItemTitleSelectionHistoryRepository(diaryItemTitleSelectionHistoryDAO)
     }
 
+    @JvmStatic
     @Singleton
     @Provides
-    @NonNull
-    public static UserPreferencesRepository provideSettingsRepository(UserPreferences userPreferences) {
-        Objects.requireNonNull(userPreferences);
-
-        UserPreferencesRepository repository = new UserPreferencesRepository(userPreferences);
-        return Objects.requireNonNull(repository);
+    fun provideSettingsRepository(userPreferences: UserPreferences): UserPreferencesRepository {
+        return UserPreferencesRepository(userPreferences)
     }
 
+    @JvmStatic
     @Singleton
     @Provides
-    @NonNull
-    public static WorkerRepository provideWorkerRepository(WorkManager workManager) {
-        Objects.requireNonNull(workManager);
-
-        WorkerRepository repository = new WorkerRepository(workManager);
-        return Objects.requireNonNull(repository);
+    fun provideWorkerRepository(workManager: WorkManager): WorkerRepository {
+        return WorkerRepository(workManager)
     }
 
+    @JvmStatic
     @Singleton
     @Provides
-    @NonNull
-    public static WeatherApiRepository provideWeatherApiRepository(WeatherApiService weatherApiService) {
-        Objects.requireNonNull(weatherApiService);
-
-        WeatherApiRepository repository = new WeatherApiRepository(weatherApiService);
-        return Objects.requireNonNull(repository);
+    fun provideWeatherApiRepository(weatherApiService: WeatherApiService): WeatherApiRepository {
+        return WeatherApiRepository(weatherApiService)
     }
 }
