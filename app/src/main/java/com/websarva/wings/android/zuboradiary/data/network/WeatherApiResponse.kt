884cd1f93bf102e0c39f521e3318c9df4cdd84e8
Takuya USAMI <tusami@gmail.com>
2025-02-04 17:25:04 +0900

    Java -> Kotlin 変換 (WeatherApiResponse)

--- app/src/main/java/com/websarva/wings/android/zuboradiary/data/network/WeatherApiResponse.kt
+++ app/src/main/java/com/websarva/wings/android/zuboradiary/data/network/WeatherApiResponse.kt
@@ -1,96 +1,51 @@
-package com.websarva.wings.android.zuboradiary.data.network;
+package com.websarva.wings.android.zuboradiary.data.network
 
-import android.util.Log;
+import android.util.Log
+import com.squareup.moshi.Json
+import com.websarva.wings.android.zuboradiary.data.diary.Weather
 
-import androidx.annotation.NonNull;
-
-import com.squareup.moshi.Json;
-import com.websarva.wings.android.zuboradiary.data.diary.Weather;
-
-import java.util.Arrays;
-import java.util.Objects;
-
-public class WeatherApiResponse {
-
+// HACK:Retrofit2(Moshi)を使用して本クラスをインスタンス化する時、引数ありのコンストラクタは処理されない。
+//      引数なしコンストラクタ処理時は、フィールド変数の値は未格納。
+//      原因が明らかになるまで、フィールド変数参照時はNullチェック等を行う。
+class WeatherApiResponse private constructor() {
     // MEMO:フィールド変数はRetrofit2(Moshi)にて代入。
-    private float latitude;
-    private float longitude;
-    private WeatherApiResponseDairy daily;
+    private var latitude = 0f
+    private var longitude = 0f
+    private lateinit var daily: WeatherApiResponseDairy
 
-    private WeatherApiResponse() {
-        // HACK:Retrofit2(Moshi)を使用して本クラスをインスタンス化する時、引数ありのコンストラクタは処理されない。
-        //      引数なしコンストラクタ処理時は、フィールド変数の値は未格納。
-        //      原因が明らかになるまで、フィールド変数参照時はNullチェック等を行う。
-    }
+    fun toWeatherInfo(): Weather {
+        // GeoCoordinatesのコンストラクタを使用してlatitude、longitudeの値チェック
+        GeoCoordinates(latitude.toDouble(), longitude.toDouble())
 
-    @NonNull
-    public Weather toWeatherInfo() {
-        new GeoCoordinates(latitude, longitude); // GeoCoordinatesのコンストラクタを使用してlatitude、longitudeの値チェック
-        Log.d("WeatherApi", "latitude:" + latitude);
-        Log.d("WeatherApi", "longitude:" + longitude);
-        Objects.requireNonNull(daily);
-        for (String s: daily.getTimes()) Log.d("WeatherApi", "time:" + s);
-        for (int i: daily.getWeatherCodes()) Log.d("WeatherApi", "weatherCode:" + i);
+        Log.d("WeatherApi", "latitude:$latitude")
+        Log.d("WeatherApi", "longitude:$longitude")
+        for (time in daily.times) Log.d("WeatherApi", "time:$time")
+        for (code in daily.weatherCodes) Log.d("WeatherApi", "weatherCode:$code")
 
-        int[] weatherCodes = daily.getWeatherCodes();
-        int weatherCode = weatherCodes[0];
-        return convertWeathers(weatherCode);
+        val weatherCodes = daily.weatherCodes
+        val weatherCode = weatherCodes[0]
+        return convertWeathers(weatherCode)
     }
 
     // "apiWeatherCode"は下記ページの"WMO 気象解釈コード"
     // https://open-meteo.com/en/docs
-    @NonNull
-    private Weather convertWeathers(int apiWeatherCode) {
-        Log.d("WeatherApi", String.valueOf(apiWeatherCode));
-        switch (apiWeatherCode) {
-            case 0:
-            case 1: case 2:
-                return Weather.SUNNY;
-            case 3:
-            case 45: case 48:
-                return Weather.CLOUDY;
-            case 51: case 53: case 55:
-            case 56: case 57:
-            case 61: case 63: case 65:
-            case 66: case 67:
-            case 80: case 81: case 82:
-            case 95:
-            case 96: case 99:
-                return Weather.RAINY;
-            case 71: case 73: case 75:
-            case 77:
-            case 85: case 86:
-                return Weather.SNOWY;
-            default:
-                return Weather.UNKNOWN;
+    private fun convertWeathers(apiWeatherCode: Int): Weather {
+        Log.d("WeatherApi", apiWeatherCode.toString())
+        return when (apiWeatherCode) {
+            0, 1, 2 -> Weather.SUNNY
+            3, 45, 48 -> Weather.CLOUDY
+            51, 53, 55, 56, 57, 61, 63, 65, 66, 67, 80, 81, 82, 95, 96, 99 -> Weather.RAINY
+            71, 73, 75, 77, 85, 86 -> Weather.SNOWY
+            else -> Weather.UNKNOWN
         }
     }
 
-    private static class WeatherApiResponseDairy {
-
+    private class WeatherApiResponseDairy {
         // MEMO:フィールド変数はRetrofit2(Moshi)にて代入。
         @Json(name = "time")
-        private String[] times;
+        lateinit var times: Array<String>
+
         @Json(name = "weather_code")
-        private int[] weatherCodes;
-
-        private WeatherApiResponseDairy() {
-            // HACK:Retrofit2(Moshi)を使用して本クラスをインスタンス化する時、引数ありのコンストラクタは処理されない。
-            //      引数なしコンストラクタ処理時は、フィールド変数の値は未格納。
-            //      原因が明らかになるまで、フィールド変数参照時はNullチェック等を行う。
-        }
-
-        private String[] getTimes() {
-            Objects.requireNonNull(times);
-            Arrays.stream(times).forEach(Objects::requireNonNull);
-
-            return times;
-        }
-
-        private int[] getWeatherCodes() {
-            Objects.requireNonNull(weatherCodes);
-
-            return weatherCodes;
-        }
+        lateinit var weatherCodes: IntArray
     }
 }
