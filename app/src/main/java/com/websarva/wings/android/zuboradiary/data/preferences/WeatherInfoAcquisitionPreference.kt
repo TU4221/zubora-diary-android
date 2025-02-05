16ac18acec269c2cab5d5120813daf48cdaab11e
Takuya USAMI <tusami@gmail.com>
2025-02-05 11:13:11 +0900

    Java -> Kotlin 変換 (WeatherInfoAcquisitionPreference)

--- app/src/main/java/com/websarva/wings/android/zuboradiary/data/preferences/WeatherInfoAcquisitionPreference.kt
+++ app/src/main/java/com/websarva/wings/android/zuboradiary/data/preferences/WeatherInfoAcquisitionPreference.kt
@@ -1,32 +1,18 @@
-package com.websarva.wings.android.zuboradiary.data.preferences;
+package com.websarva.wings.android.zuboradiary.data.preferences
 
-import androidx.datastore.preferences.core.MutablePreferences;
-import androidx.datastore.preferences.core.Preferences;
-import androidx.datastore.preferences.core.PreferencesKeys;
+import androidx.datastore.preferences.core.MutablePreferences
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
 
-import java.util.Objects;
+class WeatherInfoAcquisitionPreference @JvmOverloads constructor(val isChecked: Boolean = false) {
 
-public class WeatherInfoAcquisitionPreference {
-
-    static final Preferences.Key<Boolean> PREFERENCES_KEY_IS_CHECKED =
-            PreferencesKeys.booleanKey("is_checked_weather_info_acquisition");
-    private final boolean isChecked;
-
-    public WeatherInfoAcquisitionPreference(boolean isChecked) {
-        this.isChecked = isChecked;
+    companion object {
+        @JvmField
+        val PREFERENCES_KEY_IS_CHECKED: Preferences.Key<Boolean> =
+            booleanPreferencesKey("is_checked_weather_info_acquisition")
     }
 
-    public WeatherInfoAcquisitionPreference() {
-        this(false);
-    }
-
-    void setUpPreferences(MutablePreferences mutablePreferences) {
-        Objects.requireNonNull(mutablePreferences);
-
-        mutablePreferences.set(PREFERENCES_KEY_IS_CHECKED, isChecked);
-    }
-
-    public boolean getIsChecked() {
-        return isChecked;
+    fun setUpPreferences(mutablePreferences: MutablePreferences) {
+        mutablePreferences[PREFERENCES_KEY_IS_CHECKED] = isChecked
     }
 }
