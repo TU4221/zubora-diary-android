38845f7a59df79d4e7f5b55301d7ac2476de31db
Takuya USAMI <tusami@gmail.com>
2025-02-05 14:07:28 +0900

    Java -> Kotlin 変換 (DiaryListItem)

--- app/src/main/java/com/websarva/wings/android/zuboradiary/data/database/DiaryListItem.kt
+++ app/src/main/java/com/websarva/wings/android/zuboradiary/data/database/DiaryListItem.kt
@@ -1,29 +1,6 @@
-package com.websarva.wings.android.zuboradiary.data.database;
+package com.websarva.wings.android.zuboradiary.data.database
 
-import androidx.annotation.NonNull;
-
-public class DiaryListItem extends DiaryListBaseItem {
-
-    @NonNull
-    private String title = "";
-    @NonNull
-    private String picturePath = "";
-
-    @NonNull
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(@NonNull String title) {
-        this.title = title;
-    }
-
-    @NonNull
-    public String getPicturePath() {
-        return picturePath;
-    }
-
-    public void setPicturePath(@NonNull String picturePath) {
-        this.picturePath = picturePath;
-    }
+class DiaryListItem : DiaryListBaseItem() {
+    var title: String = ""
+    var picturePath: String = ""
 }
