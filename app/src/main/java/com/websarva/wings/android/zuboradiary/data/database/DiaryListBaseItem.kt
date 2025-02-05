e291429647fd340ad70956a1433b5afa581bf0b2
Takuya USAMI <tusami@gmail.com>
2025-02-05 14:05:13 +0900

    Java -> Kotlin 変換 (DiaryListBaseItem)

--- app/src/main/java/com/websarva/wings/android/zuboradiary/data/database/DiaryListBaseItem.kt
+++ app/src/main/java/com/websarva/wings/android/zuboradiary/data/database/DiaryListBaseItem.kt
@@ -1,19 +1,5 @@
-package com.websarva.wings.android.zuboradiary.data.database;
+package com.websarva.wings.android.zuboradiary.data.database
 
-import androidx.annotation.NonNull;
-
-public class DiaryListBaseItem {
-
-    @NonNull
-    private String date = "";
-
-    @NonNull
-    public String getDate() {
-        return this.date;
-    }
-
-    public void setDate(@NonNull String date) {
-        this.date = date;
-    }
-
+abstract class DiaryListBaseItem {
+    var date: String = ""
 }
