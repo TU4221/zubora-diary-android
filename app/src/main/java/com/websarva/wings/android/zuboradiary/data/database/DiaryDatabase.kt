c0b5d7be975c281e2c8b16564e20eea557346b0e
Takuya USAMI <tusami@gmail.com>
2025-02-05 14:41:57 +0900

    Java -> Kotlin 変換 (DiaryDatabase)

--- app/src/main/java/com/websarva/wings/android/zuboradiary/data/database/DiaryDatabase.kt
+++ app/src/main/java/com/websarva/wings/android/zuboradiary/data/database/DiaryDatabase.kt
@@ -1,20 +1,27 @@
-package com.websarva.wings.android.zuboradiary.data.database;
+package com.websarva.wings.android.zuboradiary.data.database
 
-import androidx.room.Database;
-import androidx.room.RoomDatabase;
-
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 // MEMO:テーブル構成変更手順
 //      https://qiita.com/kazuma_f/items/8c15e7087623e8f6706b
 /*@Database(entities =  {DiaryEntity.class, DiaryItemTitleSelectionHistoryItemEntity.class}, version = 4, exportSchema = true,
         autoMigrations = {@AutoMigration(from = 3, to = 4, spec = DiaryDatabase.MyAutoMigration.class)})*/
-@Database(entities =  {DiaryEntity.class, DiaryItemTitleSelectionHistoryItemEntity.class}, version = 1, exportSchema = false/*,
-        autoMigrations = {@AutoMigration(from = 2, to = 3, spec = DiaryDatabase.MyAutoMigration.class)}*/)
-public abstract class DiaryDatabase extends RoomDatabase {
-    //static class MyAutoMigration implements AutoMigrationSpec{}
-    public static final ExecutorService EXECUTOR_SERVICE = Executors.newSingleThreadExecutor();
-    public abstract DiaryDAO createDiaryDAO();
-    public abstract DiaryItemTitleSelectionHistoryDAO createDiaryItemTitleSelectionHistoryDAO();
+@Database(
+    entities = [DiaryEntity::class, DiaryItemTitleSelectionHistoryItemEntity::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class DiaryDatabase : RoomDatabase() {
+
+    companion object {
+        //static class MyAutoMigration implements AutoMigrationSpec{}
+        @JvmField
+        val EXECUTOR_SERVICE: ExecutorService = Executors.newSingleThreadExecutor()
+    }
+
+    abstract fun createDiaryDAO(): DiaryDAO
+    abstract fun createDiaryItemTitleSelectionHistoryDAO(): DiaryItemTitleSelectionHistoryDAO
 }
