f2cf65673988a7bcdfaf15bbde708d458b320ea1
Takuya USAMI <tusami@gmail.com>
2025-02-05 14:27:33 +0900

    Java -> Kotlin 変換 (DiaryItemTitleSelectionHistoryDAO)

--- app/src/main/java/com/websarva/wings/android/zuboradiary/data/database/DiaryItemTitleSelectionHistoryDAO.kt
+++ app/src/main/java/com/websarva/wings/android/zuboradiary/data/database/DiaryItemTitleSelectionHistoryDAO.kt
@@ -1,37 +1,35 @@
-package com.websarva.wings.android.zuboradiary.data.database;
+package com.websarva.wings.android.zuboradiary.data.database
 
-import androidx.room.Dao;
-import androidx.room.Insert;
-import androidx.room.OnConflictStrategy;
-import androidx.room.Query;
-
-import com.google.common.util.concurrent.ListenableFuture;
-
-import java.util.List;
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.google.common.util.concurrent.ListenableFuture
 
 @Dao
-public interface DiaryItemTitleSelectionHistoryDAO {
+interface DiaryItemTitleSelectionHistoryDAO {
     // @Query使用方法下記参照
     // https://developer.android.com/reference/kotlin/androidx/room/Query
-
     @Query("SELECT * FROM diary_item_title_selection_history ORDER BY log DESC LIMIT :numTitles OFFSET :offset")
-    ListenableFuture<List<DiaryItemTitleSelectionHistoryItemEntity>> selectHistoryListOrderByLogDesc(int numTitles, int offset);
+    fun selectHistoryListOrderByLogDesc(
+        numTitles: Int,
+        offset: Int
+    ): ListenableFuture<List<DiaryItemTitleSelectionHistoryItemEntity>>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    ListenableFuture<List<Long>> insertHistoryItem(
-            List<DiaryItemTitleSelectionHistoryItemEntity> diaryItemTitleSelectionHistoryItemEntityList);
+    fun insertHistoryItem(
+        diaryItemTitleSelectionHistoryItemEntityList: List<DiaryItemTitleSelectionHistoryItemEntity>
+    ): ListenableFuture<List<Long>>
 
     @Query("DELETE FROM diary_item_title_selection_history WHERE title = :title")
-    ListenableFuture<Integer> deleteHistoryItem(String title);
+    fun deleteHistoryItem(title: String): ListenableFuture<Int>
 
     // MEMO:SQLITEはDELETE ORDER BYが使用できない。
     /*@Query("DELETE FROM diary_item_title_history ORDER BY log DESC LIMIT ((SELECT COUNT(*) FROM diary_item_title_history) - 50) OFFSET 50")*/
-    String query = "DELETE FROM diary_item_title_selection_history WHERE title " +
-            "NOT IN (SELECT title FROM diary_item_title_selection_history ORDER BY log DESC LIMIT 50 OFFSET 0)";
-    @Query(query)
-    ListenableFuture<Integer> deleteOldHistoryItem();
+    @Query("DELETE FROM diary_item_title_selection_history WHERE title " +
+            "NOT IN (SELECT title FROM diary_item_title_selection_history ORDER BY log DESC LIMIT 50 OFFSET 0)")
+    fun deleteOldHistoryItem(): ListenableFuture<Int>
 
-    /** @noinspection UnusedReturnValue*/
     @Query("DELETE FROM diary_item_title_selection_history")
-    ListenableFuture<Void> deleteAllItem();
+    fun deleteAllItem(): ListenableFuture<Void?>
 }
