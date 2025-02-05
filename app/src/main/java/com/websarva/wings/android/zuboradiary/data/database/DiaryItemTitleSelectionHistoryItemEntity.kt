06f68af0ea67938b8d1570dddb018bab640fcf1b
Takuya USAMI <tusami@gmail.com>
2025-02-05 14:02:25 +0900

    Java -> Kotlin 変換 (DiaryItemTitleSelectionHistoryItemEntity)

--- app/src/main/java/com/websarva/wings/android/zuboradiary/data/database/DiaryItemTitleSelectionHistoryItemEntity.kt
+++ app/src/main/java/com/websarva/wings/android/zuboradiary/data/database/DiaryItemTitleSelectionHistoryItemEntity.kt
@@ -1,34 +1,11 @@
-package com.websarva.wings.android.zuboradiary.data.database;
+package com.websarva.wings.android.zuboradiary.data.database
 
-import androidx.annotation.NonNull;
-import androidx.room.Entity;
-import androidx.room.PrimaryKey;
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity(tableName = "diary_item_title_selection_history")
-public class DiaryItemTitleSelectionHistoryItemEntity {
-
+class DiaryItemTitleSelectionHistoryItemEntity {
     @PrimaryKey
-    @NonNull
-    private String title = "";
-    @NonNull
-    private String log = "";
-
-
-    @NonNull
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(@NonNull String title) {
-        this.title = title;
-    }
-
-    @NonNull
-    public String getLog() {
-        return log;
-    }
-
-    public void setLog(@NonNull String log) {
-        this.log = log;
-    }
+    var title: String = ""
+    var log: String = ""
 }
