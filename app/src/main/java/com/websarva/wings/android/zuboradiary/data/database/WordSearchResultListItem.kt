f10264b560ad36214c6196c7b4579e5e51a5cae8
Takuya USAMI <tusami@gmail.com>
2025-02-05 14:10:16 +0900

    Java -> Kotlin 変換 (WordSearchResultListItem)

--- app/src/main/java/com/websarva/wings/android/zuboradiary/data/database/WordSearchResultListItem.kt
+++ app/src/main/java/com/websarva/wings/android/zuboradiary/data/database/WordSearchResultListItem.kt
@@ -1,140 +1,38 @@
-package com.websarva.wings.android.zuboradiary.data.database;
+package com.websarva.wings.android.zuboradiary.data.database
 
-import androidx.annotation.NonNull;
-import androidx.room.ColumnInfo;
+import androidx.room.ColumnInfo
 
-public class WordSearchResultListItem extends DiaryListBaseItem {
+class WordSearchResultListItem : DiaryListBaseItem() {
 
-    @NonNull
-    private String title = "";
+    var title: String = ""
+
     @ColumnInfo(name = "item_1_title")
-    @NonNull
-    private String item1Title = "";
+    var item1Title: String = ""
+
     @ColumnInfo(name = "item_1_comment")
-    @NonNull
-    private String item1Comment = "";
+    var item1Comment: String = ""
+
     @ColumnInfo(name = "item_2_title")
-    @NonNull
-    private String item2Title = "";
+    var item2Title: String = ""
+
     @ColumnInfo(name = "item_2_comment")
-    @NonNull
-    private String item2Comment = "";
+    var item2Comment: String = ""
+
     @ColumnInfo(name = "item_3_title")
-    @NonNull
-    private String item3Title = "";
+    var item3Title: String = ""
+
     @ColumnInfo(name = "item_3_comment")
-    @NonNull
-    private String item3Comment = "";
+    var item3Comment: String = ""
+
     @ColumnInfo(name = "item_4_title")
-    @NonNull
-    private String item4Title = "";
+    var item4Title: String = ""
+
     @ColumnInfo(name = "item_4_comment")
-    @NonNull
-    private String item4Comment = "";
+    var item4Comment: String = ""
+
     @ColumnInfo(name = "item_5_title")
-    @NonNull
-    private String item5Title = "";
+    var item5Title: String = ""
+
     @ColumnInfo(name = "item_5_comment")
-    @NonNull
-    private String item5Comment = "";
-
-    @NonNull
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(@NonNull String title) {
-        this.title = title;
-    }
-
-    @NonNull
-    public String getItem1Title() {
-        return item1Title;
-    }
-
-    public void setItem1Title(@NonNull String item1Title) {
-        this.item1Title = item1Title;
-    }
-
-    @NonNull
-    public String getItem1Comment() {
-        return item1Comment;
-    }
-
-    public void setItem1Comment(@NonNull String item1Comment) {
-        this.item1Comment = item1Comment;
-    }
-
-    @NonNull
-    public String getItem2Title() {
-        return item2Title;
-    }
-
-    public void setItem2Title(@NonNull String item2Title) {
-        this.item2Title = item2Title;
-    }
-
-    @NonNull
-    public String getItem2Comment() {
-        return item2Comment;
-    }
-
-    public void setItem2Comment(@NonNull String item2Comment) {
-        this.item2Comment = item2Comment;
-    }
-
-    @NonNull
-    public String getItem3Title() {
-        return item3Title;
-    }
-
-    public void setItem3Title(@NonNull String item3Title) {
-        this.item3Title = item3Title;
-    }
-
-    @NonNull
-    public String getItem3Comment() {
-        return item3Comment;
-    }
-
-    public void setItem3Comment(@NonNull String item3Comment) {
-        this.item3Comment = item3Comment;
-    }
-
-    @NonNull
-    public String getItem4Title() {
-        return item4Title;
-    }
-
-    public void setItem4Title(@NonNull String item4Title) {
-        this.item4Title = item4Title;
-    }
-
-    @NonNull
-    public String getItem4Comment() {
-        return item4Comment;
-    }
-
-    public void setItem4Comment(@NonNull String item4Comment) {
-        this.item4Comment = item4Comment;
-    }
-
-    @NonNull
-    public String getItem5Title() {
-        return item5Title;
-    }
-
-    public void setItem5Title(@NonNull String item5Title) {
-        this.item5Title = item5Title;
-    }
-
-    @NonNull
-    public String getItem5Comment() {
-        return item5Comment;
-    }
-
-    public void setItem5Comment(@NonNull String item5Comment) {
-        this.item5Comment = item5Comment;
-    }
-
+    var item5Comment: String = ""
 }
