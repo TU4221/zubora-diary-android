64ff0a7a7724b9c965c19cc4e6e83c112477a90d
Takuya USAMI <tusami@gmail.com>
2025-04-15 10:56:39 +0900

    クラス名変更 CustomTextInputEditText-> OneTouchClickTextInputEditText

--- app/src/main/java/com/websarva/wings/android/zuboradiary/ui/view/custom/OneTouchClickTextInputEditText.kt
+++ app/src/main/java/com/websarva/wings/android/zuboradiary/ui/view/custom/OneTouchClickTextInputEditText.kt
@@ -14,7 +14,7 @@ import com.google.android.material.textfield.TextInputEditText
 //      これは本来のアクセシビリティサービスが損なわれる可能性があることを示唆している。
 //      対策としてonTouchEvent()メソッドをOverrideした本クラスを作成して使用。
 //      https://www.binarydevelop.com/article/androidsetontouchlistenerperformclick-68009
-internal class CustomTextInputEditText : TextInputEditText {
+internal class OneTouchClickTextInputEditText : TextInputEditText {
     constructor(context: Context) : super(context)
 
     constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
