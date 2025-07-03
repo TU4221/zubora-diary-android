d1616b450eae43822db52a41ca3980d1fad74786
Takuya USAMI <tusami@gmail.com>
2025-07-03 11:16:04 +0900

    CalendarState コメント、コード並び修正

--- app/src/main/java/com/websarva/wings/android/zuboradiary/ui/model/state/CalendarState.kt
+++ app/src/main/java/com/websarva/wings/android/zuboradiary/ui/model/state/CalendarState.kt
@@ -3,10 +3,10 @@ package com.websarva.wings.android.zuboradiary.ui.model.state
 internal sealed class CalendarState : UiState {
     data object Idle: CalendarState() // 初期状態
 
-    data object LoadingDiaryInfo : CalendarState() //日記読込中
+    data object LoadingDiaryInfo : CalendarState() //日記情報読込中
     data object LoadingDiary : CalendarState() //日記読込中
-    data object LoadDiarySuccess : CalendarState() //日記読込中
-    data object LoadError : CalendarState()
 
+    data object LoadDiarySuccess : CalendarState() //日記読込成功
+    data object LoadError : CalendarState() //日記読込失敗
     data object NoDiary : CalendarState() // 日記非表示
 }
