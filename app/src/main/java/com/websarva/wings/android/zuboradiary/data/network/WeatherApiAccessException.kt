82f21b21136cce48318959c6ec919df6f351b94b
Takuya USAMI <tusami@gmail.com>
2025-07-03 11:51:27 +0900

    未使用クラス削除

--- app/src/main/java/com/websarva/wings/android/zuboradiary/data/network/WeatherApiAccessException.kt
+++
@@ -1,7 +0,0 @@
-package com.websarva.wings.android.zuboradiary.data.network
-
-import com.websarva.wings.android.zuboradiary.data.model.DataException
-
-internal class WeatherApiAccessException (
-    cause: Throwable? = null
-) : DataException("天気情報の取得に失敗しました。", cause)
