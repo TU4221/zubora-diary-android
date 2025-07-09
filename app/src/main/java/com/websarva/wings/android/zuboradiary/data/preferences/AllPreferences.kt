4475701689aef8231f27c521d9657f97fb9b94b4
Takuya USAMI <tusami@gmail.com>
2025-07-09 15:22:22 +0900

    UserPreferences関係 不要クラス削除

--- app/src/main/java/com/websarva/wings/android/zuboradiary/data/preferences/AllPreferences.kt
+++
@@ -1,9 +0,0 @@
-package com.websarva.wings.android.zuboradiary.data.preferences
-
-internal data class AllPreferences(
-    val themeColorPreference: ThemeColorPreference,
-    val calendarStartDayOfWeekPreference: CalendarStartDayOfWeekPreference,
-    val reminderNotificationPreference: ReminderNotificationPreference,
-    val passcodeLockPreference: PassCodeLockPreference,
-    val weatherInfoFetchPreference: WeatherInfoFetchPreference,
-) : UserPreference
--- app/src/main/java/com/websarva/wings/android/zuboradiary/data/preferences/UserPreferencesLoadingResult.kt
+++
@@ -1,9 +0,0 @@
-package com.websarva.wings.android.zuboradiary.data.preferences
-
-internal sealed class UserPreferencesLoadingResult {
-    data class Success(val preferences: AllPreferences) : UserPreferencesLoadingResult()
-    data class Failure(
-        val exception: UserPreferencesAccessException,
-        val fallbackPreferences: AllPreferences
-    ) : UserPreferencesLoadingResult()
-}
