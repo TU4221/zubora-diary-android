9a361b497d0d1bf636123fda9c33be3466fd3af1
Takuya USAMI <tusami@gmail.com>
2025-07-28 13:44:59 +0900

    NavigatePreviousParameters 不要コメント削除

--- app/src/main/java/com/websarva/wings/android/zuboradiary/ui/model/parameters/NavigatePreviousParameters.kt
+++ app/src/main/java/com/websarva/wings/android/zuboradiary/ui/model/parameters/NavigatePreviousParameters.kt
@@ -3,7 +3,6 @@ package com.websarva.wings.android.zuboradiary.ui.model.parameters
 import com.websarva.wings.android.zuboradiary.domain.model.Diary
 import java.io.Serializable
 
-// MEMO:日記表示、編集フラグメント以外からも削除できるように下記データ構成とする。
 internal data class NavigatePreviousParameters(
     val originalDiary: Diary?
 ) : Serializable
