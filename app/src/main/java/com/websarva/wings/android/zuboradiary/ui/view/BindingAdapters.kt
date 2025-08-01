3c54362a672088d9f2ca049be29b8b4b5190e1e2
Takuya USAMI <tusami@gmail.com>
2025-08-01 21:03:54 +0900

    BindingAdapter `applyStatusBarTopPadding`を削除

--- app/src/main/java/com/websarva/wings/android/zuboradiary/ui/view/BindingAdapters.kt
+++ app/src/main/java/com/websarva/wings/android/zuboradiary/ui/view/BindingAdapters.kt
@@ -7,7 +7,6 @@ import android.widget.TextView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updateLayoutParams
-import androidx.core.view.updatePadding
 import androidx.databinding.BindingAdapter
 import com.google.android.material.appbar.MaterialToolbar
 
@@ -28,40 +27,6 @@ internal object BindingAdapters {
     @BindingAdapter("onNavigationIconClick")
     fun setOnNavigationIconClickListener(toolbar: MaterialToolbar, listener: View.OnClickListener) {
         toolbar.setNavigationOnClickListener(listener)
-    }
-
-    /**
-     * 対象Viewの上部に、ステータスバーの高さ分のパディングを適用します。
-     * 主にエッジ・トゥ・エッジ表示が有効な場合に使用されます。
-     *
-     * このアダプターは、Viewに `fitsSystemWindows = true` を設定せずに、
-     * ウィンドウインセットのリスナーのみを登録します。リスナー内で、Viewの上部パディングを
-     * ステータスバーの高さで更新し、インセットを消費します。
-     *
-     * 用途：
-     * 1. エッジ・トゥ・エッジ環境下で、Toolbar などのビューがステータスバーと重ならないようにする。
-     * 2. `fitsSystemWindows = true` を使用した際に発生する、Toolbar の高さが意図せず
-     *    変わってしまう問題（特に `?attr/actionBarSize` との組み合わせ時）を回避する代替手段として、
-     *    トップパディングのみを精密に制御する。
-     *
-     *
-     * @param view パディングを適用する対象のビュー。
-     * @param apply true の場合、ステータスバー上部のパディングを適用します。false の場合は何もしません。
-     * */
-    @JvmStatic
-    @BindingAdapter("applyStatusBarTopPadding")
-    fun applyStatusBarTopPadding(view: View, apply: Boolean) {
-        if (!apply) return
-
-        view.apply {
-            ViewCompat.setOnApplyWindowInsetsListener(
-                this
-            ) { view, insets ->
-                val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-                view.updatePadding(top = systemBars.top)
-                WindowInsetsCompat.CONSUMED
-            }
-        }
     }
 
     @JvmStatic
