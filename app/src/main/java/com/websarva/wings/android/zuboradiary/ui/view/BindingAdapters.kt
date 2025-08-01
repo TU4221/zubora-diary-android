c7ed24e5c9a030c798e840b0802b6fc6c83cf077
Takuya USAMI <tusami@gmail.com>
2025-08-01 21:02:52 +0900

    WordSearchFragment FABのBottomMarginにNavigationBarの高さを適用

--- app/src/main/java/com/websarva/wings/android/zuboradiary/ui/view/BindingAdapters.kt
+++ app/src/main/java/com/websarva/wings/android/zuboradiary/ui/view/BindingAdapters.kt
@@ -1,7 +1,6 @@
 package com.websarva.wings.android.zuboradiary.ui.view
 
 import android.graphics.drawable.Drawable
-import android.util.Log
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
