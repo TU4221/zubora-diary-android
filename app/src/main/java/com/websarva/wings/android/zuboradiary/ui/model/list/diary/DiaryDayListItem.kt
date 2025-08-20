04a2f52541d20e81b6f241ca9e85c83578b17075
Takuya USAMI <tusami@gmail.com>
2025-08-20 17:50:04 +0900

    `createSpannableString`系関数を`WordSearchResultDayListAdapter`クラスへ移動

--- app/src/main/java/com/websarva/wings/android/zuboradiary/ui/model/list/diary/DiaryDayListItem.kt
+++ app/src/main/java/com/websarva/wings/android/zuboradiary/ui/model/list/diary/DiaryDayListItem.kt
@@ -1,13 +1,7 @@
 package com.websarva.wings.android.zuboradiary.ui.model.list.diary
 
-import android.content.Context
 import android.net.Uri
-import android.text.SpannableString
-import android.text.Spanned
-import android.text.style.BackgroundColorSpan
-import android.text.style.ForegroundColorSpan
 import com.websarva.wings.android.zuboradiary.domain.model.ItemNumber
-import com.websarva.wings.android.zuboradiary.domain.model.ThemeColor
 import java.time.LocalDate
 
 internal sealed class DiaryDayListItem(
@@ -27,52 +21,5 @@ internal sealed class DiaryDayListItem(
         val itemTitle: String,
         val itemComment: String,
         val searchWord: String,
-    ) : DiaryDayListItem(date) {
-
-        // TODO:下記関数は最終的にViewHolderへ以降
-        fun createTitleSpannableString(context: Context, themeColor: ThemeColor): SpannableString {
-            return toSpannableString(context, title, searchWord, themeColor)
-        }
-
-        fun createItemTitleSpannableString(context: Context, themeColor: ThemeColor): SpannableString {
-            return toSpannableString(context, itemTitle, searchWord, themeColor)
-        }
-
-        fun createItemCommentSpannableString(context: Context, themeColor: ThemeColor): SpannableString {
-            return toSpannableString(context, itemComment, searchWord, themeColor)
-        }
-
-        // 対象ワードをマーキング
-        private fun toSpannableString(
-            context: Context,
-            string: String,
-            targetWord: String,
-            themeColor: ThemeColor
-        ): SpannableString {
-            val spannableString = SpannableString(string)
-            var fromIndex = 0
-            while (string.indexOf(targetWord, fromIndex) != -1) {
-                val textColor = themeColor.getOnTertiaryContainerColor(context.resources)
-                val backgroundColor = themeColor.getTertiaryContainerColor(context.resources)
-                val backgroundColorSpan = BackgroundColorSpan(backgroundColor)
-                val foregroundColorSpan = ForegroundColorSpan(textColor)
-                val start = string.indexOf(targetWord, fromIndex)
-                val end = start + targetWord.length
-                spannableString.setSpan(
-                    backgroundColorSpan,
-                    start,
-                    end,
-                    Spanned.SPAN_INCLUSIVE_INCLUSIVE
-                )
-                spannableString.setSpan(
-                    foregroundColorSpan,
-                    start,
-                    end,
-                    Spanned.SPAN_INCLUSIVE_INCLUSIVE
-                )
-                fromIndex = end
-            }
-            return spannableString
-        }
-    }
+    ) : DiaryDayListItem(date)
 }
