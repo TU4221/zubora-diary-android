2efb2d22b9f90e3d282d7ea9736e1ede35f33237
Takuya USAMI <tusami@gmail.com>
2025-08-29 19:18:31 +0900

    DataBaseAccessFailureException 引数causeを非Null許容型に変更

--- app/src/main/java/com/websarva/wings/android/zuboradiary/data/database/DataBaseAccessFailureException.kt
+++ app/src/main/java/com/websarva/wings/android/zuboradiary/data/database/DataBaseAccessFailureException.kt
@@ -8,5 +8,5 @@ package com.websarva.wings.android.zuboradiary.data.database
  * @param cause データベースアクセス失敗の根本原因となったThrowable (オプショナル)。
  */
 internal class DataBaseAccessFailureException (
-    cause: Throwable? = null //TODO:非Null型に変更
+    cause: Throwable
 ) : Exception("データベースへのアクセスに失敗しました。", cause)
