a904d3db6b8ef8fc72500b0d768c9640caa34cc5
Takuya USAMI <tusami@gmail.com>
2025-08-30 15:38:59 +0900

    `WeatherApiException`のcause引数を見直し、

--- app/src/main/java/com/websarva/wings/android/zuboradiary/data/network/WeatherApiException.kt
+++ app/src/main/java/com/websarva/wings/android/zuboradiary/data/network/WeatherApiException.kt
@@ -2,17 +2,17 @@ package com.websarva.wings.android.zuboradiary.data.network
 
 import java.time.LocalDate
 
+
 internal sealed class WeatherApiException (
     message: String,
     cause: Throwable? = null
 ) : Exception(message, cause) {
 
     class ApiAccessFailure (
-        cause: Throwable? = null
+        cause: Throwable
     ) : WeatherApiException("天気情報取得Apiのアクセスに失敗しました。", cause)
 
     class DateOutOfRange (
-        date: LocalDate,
-        cause: Throwable? = null
-    ) : WeatherApiException("指定した日付 '$date' は、天気情報を取得できる範囲を超えています。", cause)
+        date: LocalDate
+    ) : WeatherApiException("指定した日付 '$date' は、天気情報を取得できる範囲を超えています。")
 }
