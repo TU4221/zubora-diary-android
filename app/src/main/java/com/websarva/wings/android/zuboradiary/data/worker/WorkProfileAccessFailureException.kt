486129d44d951d0a800854fc9976bcab8065a34f
Takuya USAMI <tusami@gmail.com>
2025-08-31 13:55:53 +0900

    WorkProfileAccessFailureException 引数causeを非Null許容型に変更

--- app/src/main/java/com/websarva/wings/android/zuboradiary/data/worker/WorkProfileAccessFailureException.kt
+++ app/src/main/java/com/websarva/wings/android/zuboradiary/data/worker/WorkProfileAccessFailureException.kt
@@ -1,5 +1,5 @@
 package com.websarva.wings.android.zuboradiary.data.worker
 
 internal class WorkProfileAccessFailureException (
-    cause: Throwable? = null
+    cause: Throwable
 ) : Exception("仕事用プロファイルへのアクセスまたは操作に失敗しました。", cause)
