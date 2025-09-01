e3f5b002661792005e458dc544b5546e41c79eb9
Takuya USAMI <tusami@gmail.com>
2025-09-01 23:36:12 +0900

    UserSettingsLoadException.DataNotFound時にdefaultSettingValueを返すように修正

--- app/src/main/java/com/websarva/wings/android/zuboradiary/domain/usecase/settings/LoadCalendarStartDayOfWeekSettingUseCase.kt
+++ app/src/main/java/com/websarva/wings/android/zuboradiary/domain/usecase/settings/LoadCalendarStartDayOfWeekSettingUseCase.kt
@@ -40,7 +40,7 @@ internal class LoadCalendarStartDayOfWeekSettingUseCase(
                                 }
                                 is UserSettingsLoadException.DataNotFound -> {
                                     UserSettingResult.Success(
-                                        CalendarStartDayOfWeekSetting() // TODO:初期値defaultSettingValue代入
+                                        defaultSettingValue
                                     )
                                 }
                             }
