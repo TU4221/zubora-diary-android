3ba64ed9c7c52785fb6479bd5d53be3dbdfb2e24
Takuya USAMI <tusami@gmail.com>
2025-09-01 10:39:44 +0900

    CoroutineScopesModuleにKDoC追記

--- app/src/main/java/com/websarva/wings/android/zuboradiary/di/CoroutineScopesModule.kt
+++ app/src/main/java/com/websarva/wings/android/zuboradiary/di/CoroutineScopesModule.kt
@@ -10,14 +10,28 @@ import kotlinx.coroutines.SupervisorJob
 import javax.inject.Qualifier
 import javax.inject.Singleton
 
+/**
+ * アプリケーション全体で使用するコルーチンスコープを識別するためのカスタム修飾子アノテーション。
+ */
 @Retention(AnnotationRetention.BINARY)
 @Qualifier
-annotation class ApplicationScope // カスタム修飾子
+annotation class ApplicationScope
 
+/**
+ * アプリケーション全体で共有されるコルーチンスコープを提供するHiltモジュール。
+ *
+ * このモジュールは、[SingletonComponent] にインストールされ、アプリケーションのライフサイクルと
+ * 同じ期間生存する [CoroutineScope] を提供する。
+ */
 @Module
 @InstallIn(SingletonComponent::class)
 internal object CoroutineScopesModule {
 
+    /**
+     * アプリケーションスコープの [CoroutineScope] を提供する。
+     *
+     * @return アプリケーション全体で共有されるシングルトンの [CoroutineScope] インスタンス。
+     */
     @ApplicationScope
     @Singleton
     @Provides
