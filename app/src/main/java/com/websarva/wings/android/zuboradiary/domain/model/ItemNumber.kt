8582a27ef0ffebb7990134adc15f962e49956afd
Takuya USAMI <tusami@gmail.com>
2025-09-01 23:08:03 +0900

    ItemNumberクラスをValue Classに変更

--- app/src/main/java/com/websarva/wings/android/zuboradiary/domain/model/ItemNumber.kt
+++ app/src/main/java/com/websarva/wings/android/zuboradiary/domain/model/ItemNumber.kt
@@ -2,21 +2,17 @@ package com.websarva.wings.android.zuboradiary.domain.model
 
 import java.io.Serializable
 
-// TODO:ValueClassに変更
-internal class ItemNumber(value: Int) : Serializable, Comparable<ItemNumber> {
+@JvmInline
+internal value class ItemNumber(val value: Int) : Serializable, Comparable<ItemNumber> {
 
     companion object {
         const val MIN_NUMBER: Int = 1
         const val MAX_NUMBER: Int = 5
     }
 
-    val value: Int
-
     init {
         require(value >= MIN_NUMBER)
         require(value <= MAX_NUMBER)
-
-        this.value = value
     }
 
     fun inc(): ItemNumber {
@@ -26,21 +22,7 @@ internal class ItemNumber(value: Int) : Serializable, Comparable<ItemNumber> {
         return ItemNumber(value)
     }
 
-    override fun toString(): String {
-        return value.toString()
-    }
-
-    override fun equals(other: Any?): Boolean {
-        if (other == null) return false
-        if (other !is ItemNumber) return false
-        return this.value == other.value
-    }
-
     override fun compareTo(other: ItemNumber): Int {
         return this.value.compareTo(other.value)
-    }
-
-    override fun hashCode(): Int {
-        return value
     }
 }
