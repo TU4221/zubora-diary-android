b1ade49bed92a9eca877602f78aea3274a40c1fa
Takuya USAMI <tusami@gmail.com>
2025-09-04 14:30:10 +0900

    WordSearchResultDayListAdapterのitemNumber取得方法修正

--- app/src/main/java/com/websarva/wings/android/zuboradiary/ui/adapter/recycler/diary/wordsearchresult/WordSearchResultDayListAdapter.kt
+++ app/src/main/java/com/websarva/wings/android/zuboradiary/ui/adapter/recycler/diary/wordsearchresult/WordSearchResultDayListAdapter.kt
@@ -99,7 +99,7 @@ internal class WordSearchResultDayListAdapter(
                 textTitle.text = title
 
                 val strItemNumber =
-                    context.getString(R.string.fragment_word_search_result_item) + item.itemNumber
+                    context.getString(R.string.fragment_word_search_result_item) + item.itemNumber.value
                 textItemNumber.text = strItemNumber
                 textItemTitle.text = item.createItemTitleSpannableString(context, themeColor)
                 textItemComment.text = item.createItemCommentSpannableString(context, themeColor)
