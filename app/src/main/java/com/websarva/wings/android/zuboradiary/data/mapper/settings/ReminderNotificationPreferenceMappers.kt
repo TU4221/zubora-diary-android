fd96289f0ae53ff3185bbfb5a6bed002b9f9583c
Takuya USAMI <tusami@gmail.com>
2025-09-30 16:06:20 +0900

    ReminderNotificationPreference.toDomainModel()の不正なnotificationTimeStringでDateTimeExceptionが発生する問題を修正(上位で対応するように)

--- app/src/main/java/com/websarva/wings/android/zuboradiary/data/mapper/settings/ReminderNotificationPreferenceMappers.kt
+++ app/src/main/java/com/websarva/wings/android/zuboradiary/data/mapper/settings/ReminderNotificationPreferenceMappers.kt
@@ -1,21 +1,13 @@
 package com.websarva.wings.android.zuboradiary.data.mapper.settings
 
-import android.util.Log
 import com.websarva.wings.android.zuboradiary.data.preferences.ReminderNotificationPreference
 import com.websarva.wings.android.zuboradiary.domain.model.settings.ReminderNotificationSetting
-import com.websarva.wings.android.zuboradiary.utils.createLogTag
-import java.time.DateTimeException
 import java.time.LocalTime
 
 internal fun ReminderNotificationPreference.toDomainModel(): ReminderNotificationSetting {
     return if (isEnabled) {
-        try {
-            val notificationTime = LocalTime.parse(notificationTimeString)
-            ReminderNotificationSetting.Enabled(notificationTime)
-        } catch (e: DateTimeException) {
-            Log.e(createLogTag(), "リマインダー通知が有効ですが、通知時刻文字列が空なので、無効状態で返します。")
-            ReminderNotificationSetting.Disabled
-        }
+        val notificationTime = LocalTime.parse(notificationTimeString)
+        ReminderNotificationSetting.Enabled(notificationTime)
     } else {
         ReminderNotificationSetting.Disabled
     }
