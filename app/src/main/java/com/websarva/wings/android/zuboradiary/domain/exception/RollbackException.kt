720db39b019e3f1c50c4dcce0b0e3c2c836c6ef2
Takuya USAMI <tusami@gmail.com>
2025-09-30 09:41:54 +0900

    RollbackExceptionのKDoc修正

--- app/src/main/java/com/websarva/wings/android/zuboradiary/domain/exception/RollbackException.kt
+++ app/src/main/java/com/websarva/wings/android/zuboradiary/domain/exception/RollbackException.kt
@@ -1,12 +1,11 @@
 package com.websarva.wings.android.zuboradiary.domain.exception
 
-// TODO:RepositoryExceptionをRepositoryレベルではなくDomainレベルに変更する？
 /**
  * ロールバック処理に問題が発生した場合の例外。
  *
  *  @param message この例外に関する詳細メッセージ。エラーの原因や状況を簡潔に説明する。
  *                デフォルトは「ロールバック処理に問題が発生。」
- * @param cause この例外の根本原因となった[Throwable]。データ層で発生したスケジューリング関連例外を含む。
+ * @param cause この例外の根本原因となった[Throwable]。
  */
 internal class RollbackException(
     message: String = "ロールバック処理に問題が発生。",
