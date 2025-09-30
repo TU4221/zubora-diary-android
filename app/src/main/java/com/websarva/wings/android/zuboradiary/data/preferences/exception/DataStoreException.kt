1add6d1dd28266bd9e37bfced9f5796e077f84ca
Takuya USAMI <tusami@gmail.com>
2025-09-30 16:57:47 +0900

    DataStoreException TODOコメント削除

--- app/src/main/java/com/websarva/wings/android/zuboradiary/data/preferences/exception/DataStoreException.kt
+++ app/src/main/java/com/websarva/wings/android/zuboradiary/data/preferences/exception/DataStoreException.kt
@@ -2,7 +2,6 @@ package com.websarva.wings.android.zuboradiary.data.preferences.exception
 
 import com.websarva.wings.android.zuboradiary.data.exception.StorageException
 
-// TODO:サブクラス未使用(既存例外と置き換え必須。サブクラスは仮のため置き換え時に内容を確認すること)
 /**
  * データストア操作 (DataStore Preferences) 操作に関連する問題を示す例外。
  * [StorageException] を継承。
