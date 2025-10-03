70f3702e279688bbe52a2d3ef573b38cd6f195a8
Takuya USAMI <tusami@gmail.com>
2025-10-03 15:18:22 +0900

    ImageFileNameUiの型をFileNameからStringに変更(Fragment層からドメインモデル除去)

--- app/src/main/java/com/websarva/wings/android/zuboradiary/ui/mapper/ImageFileNameMapper.kt
+++ app/src/main/java/com/websarva/wings/android/zuboradiary/ui/mapper/ImageFileNameMapper.kt
@@ -4,9 +4,9 @@ import com.websarva.wings.android.zuboradiary.domain.model.FileName
 import com.websarva.wings.android.zuboradiary.ui.model.ImageFileNameUi
 
 internal fun FileName.toUiModel(): ImageFileNameUi {
-    return ImageFileNameUi(this)
+    return ImageFileNameUi(fullName)
 }
 
 internal fun ImageFileNameUi.toDomainModel(): FileName {
-    return fullName
+    return FileName(fullName)
 }
--- app/src/main/java/com/websarva/wings/android/zuboradiary/ui/model/ImageFileNameUi.kt
+++ app/src/main/java/com/websarva/wings/android/zuboradiary/ui/model/ImageFileNameUi.kt
@@ -1,6 +1,4 @@
 package com.websarva.wings.android.zuboradiary.ui.model
 
-import com.websarva.wings.android.zuboradiary.domain.model.FileName
-
 @JvmInline
-internal value class ImageFileNameUi(val fullName: FileName)
+internal value class ImageFileNameUi(val fullName: String)
