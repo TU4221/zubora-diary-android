fd84c2781c140b19ba90a1618652bc96f22a8aae
Takuya USAMI <tusami@gmail.com>
2025-11-02 16:34:11 +0900

    BaseViewModelからstateInWhileSubscribedの拡張関数を不要の為削除

--- app/src/main/java/com/websarva/wings/android/zuboradiary/ui/viewmodel/common/BaseViewModel.kt
+++ app/src/main/java/com/websarva/wings/android/zuboradiary/ui/viewmodel/common/BaseViewModel.kt
@@ -18,12 +18,9 @@ import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import kotlin.coroutines.cancellation.CancellationException
@@ -31,16 +28,6 @@ import kotlin.coroutines.cancellation.CancellationException
 internal abstract class BaseViewModel<E: UiEvent, M: AppMessage, S: UiState>(
     initialViewUiState: S
 ) : ViewModel() {
-
-    fun <T> Flow<T>.stateInWhileSubscribed(
-        initialValue: T
-    ): StateFlow<T> {
-        return this.stateIn(
-            viewModelScope,
-            SharingStarted.WhileSubscribed(5000L),
-            initialValue
-        )
-    }
     
     private val logMsgPendingNavi = "保留ナビゲーション_"
 
