b91618cd98eadb8dee1e1c9e640372b5b86ea68d
Takuya USAMI <tusami@gmail.com>
2025-11-04 17:13:31 +0900

    FragmentResult, DialogResultのコメントをHACKに変更

--- app/src/main/java/com/websarva/wings/android/zuboradiary/ui/model/result/DialogResult.kt
+++ app/src/main/java/com/websarva/wings/android/zuboradiary/ui/model/result/DialogResult.kt
@@ -2,8 +2,8 @@ package com.websarva.wings.android.zuboradiary.ui.model.result
 
 import java.io.Serializable
 
-// MEMO:Parcelableを実装するとポジティブデータの型(ジェネリクスの型)がParcelableのみしか受け付けなくなるため、
-//      Serializableで対応。
+// HACK:Parcelableを実装するとポジティブデータの型(ジェネリクスの型)がParcelableのみしか受け付けなくなるため、
+//      Serializableで対応。(ParcelableだとUnitが指定できなくなる)
 sealed class DialogResult<out T> : NavigationResult, Serializable {
 
     data class Positive<out T>(
@@ -11,10 +11,14 @@ sealed class DialogResult<out T> : NavigationResult, Serializable {
     ) : DialogResult<T>()
 
     data object Negative : DialogResult<Nothing>() {
+        // HACK:Serializable実装すると下記を記述するように促されるが、未使用の為、下記アノテーションで警告抑制
+        @Suppress("unused")
         private fun readResolve(): Any = Negative
     }
 
     data object Cancel : DialogResult<Nothing>() {
+        // HACK:Serializable実装すると下記を記述するように促されるが、未使用の為、下記アノテーションで警告抑制
+        @Suppress("unused")
         private fun readResolve(): Any = Cancel
     }
 }
--- app/src/main/java/com/websarva/wings/android/zuboradiary/ui/model/result/FragmentResult.kt
+++ app/src/main/java/com/websarva/wings/android/zuboradiary/ui/model/result/FragmentResult.kt
@@ -2,8 +2,8 @@ package com.websarva.wings.android.zuboradiary.ui.model.result
 
 import java.io.Serializable
 
-// MEMO:Parcelableを実装するとポジティブデータの型(ジェネリクスの型)がParcelableのみしか受け付けなくなるため、
-//      Serializableで対応。
+// HACK:Parcelableを実装するとポジティブデータの型(ジェネリクスの型)がParcelableのみしか受け付けなくなるため、
+//      Serializableで対応。(ParcelableだとUnitが指定できなくなる)
 sealed class FragmentResult<out T> : NavigationResult, Serializable {
 
     data class Some<out T>(
@@ -11,6 +11,8 @@ sealed class FragmentResult<out T> : NavigationResult, Serializable {
     ) : FragmentResult<T>()
 
     data object None : FragmentResult<Nothing>() {
+        // HACK:Serializable実装すると下記を記述するように促されるが、未使用の為、下記アノテーションで警告抑制
+        @Suppress("unused")
         private fun readResolve(): Any = None
     }
 }
