0546a14f97b6656fb90e4e325b9aba28fd9390f4
Takuya USAMI <tusami@gmail.com>
2025-11-16 23:31:55 +0900

    DayOfWeek関連のStringリソースID名を変更

--- app/src/main/java/com/websarva/wings/android/zuboradiary/ui/utils/DayOfWeekExtensions.kt
+++ app/src/main/java/com/websarva/wings/android/zuboradiary/ui/utils/DayOfWeekExtensions.kt
@@ -6,26 +6,26 @@ import java.time.DayOfWeek
 
 internal fun DayOfWeek.asCalendarStartDayOfWeekString(context: Context): String {
     val resId = when (this) {
-        DayOfWeek.SUNDAY -> R.string.day_of_week_name_sunday
-        DayOfWeek.MONDAY -> R.string.day_of_week_name_monday
-        DayOfWeek.TUESDAY -> R.string.day_of_week_name_tuesday
-        DayOfWeek.WEDNESDAY -> R.string.day_of_week_name_wednesday
-        DayOfWeek.THURSDAY -> R.string.day_of_week_name_thursday
-        DayOfWeek.FRIDAY -> R.string.day_of_week_name_friday
-        DayOfWeek.SATURDAY -> R.string.day_of_week_name_saturday
+        DayOfWeek.SUNDAY -> R.string.enum_day_of_week_name_sunday
+        DayOfWeek.MONDAY -> R.string.enum_day_of_week_name_monday
+        DayOfWeek.TUESDAY -> R.string.enum_day_of_week_name_tuesday
+        DayOfWeek.WEDNESDAY -> R.string.enum_day_of_week_name_wednesday
+        DayOfWeek.THURSDAY -> R.string.enum_day_of_week_name_thursday
+        DayOfWeek.FRIDAY -> R.string.enum_day_of_week_name_friday
+        DayOfWeek.SATURDAY -> R.string.enum_day_of_week_name_saturday
     }
     return context.getString(resId)
 }
 
 internal fun DayOfWeek.asDiaryListDayOfWeekString(context: Context): String {
     val resId = when (this) {
-        DayOfWeek.SUNDAY -> R.string.day_of_week_short_name_sunday
-        DayOfWeek.MONDAY -> R.string.day_of_week_short_name_monday
-        DayOfWeek.TUESDAY -> R.string.day_of_week_short_name_tuesday
-        DayOfWeek.WEDNESDAY -> R.string.day_of_week_short_name_wednesday
-        DayOfWeek.THURSDAY -> R.string.day_of_week_short_name_thursday
-        DayOfWeek.FRIDAY -> R.string.day_of_week_short_name_friday
-        DayOfWeek.SATURDAY -> R.string.day_of_week_short_name_saturday
+        DayOfWeek.SUNDAY -> R.string.enum_day_of_week_short_name_sunday
+        DayOfWeek.MONDAY -> R.string.enum_day_of_week_short_name_monday
+        DayOfWeek.TUESDAY -> R.string.enum_day_of_week_short_name_tuesday
+        DayOfWeek.WEDNESDAY -> R.string.enum_day_of_week_short_name_wednesday
+        DayOfWeek.THURSDAY -> R.string.enum_day_of_week_short_name_thursday
+        DayOfWeek.FRIDAY -> R.string.enum_day_of_week_short_name_friday
+        DayOfWeek.SATURDAY -> R.string.enum_day_of_week_short_name_saturday
     }
     return context.getString(resId)
 }
