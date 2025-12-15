16f2324496e56a4746ba7b751d8f28aee37aa83b
Takuya USAMI <tusami@gmail.com>
2025-12-15 13:46:39 +0900

    WhetherInfoFetchSetting class -> data class へ修正

--- app/src/main/java/com/websarva/wings/android/zuboradiary/domain/model/settings/WeatherInfoFetchSetting.kt
+++ app/src/main/java/com/websarva/wings/android/zuboradiary/domain/model/settings/WeatherInfoFetchSetting.kt
@@ -7,7 +7,7 @@ package com.websarva.wings.android.zuboradiary.domain.model.settings
  *
  * @property isEnabled 天気情報の自動取得が有効な場合は `true`、無効な場合は `false`。
  */
-internal class WeatherInfoFetchSetting(
+internal data class WeatherInfoFetchSetting(
     val isEnabled: Boolean
 ) : UserSetting {
     companion object {
