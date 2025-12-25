6f5391f8467ad9c702d033953e6c5c21e59faed2
Takuya USAMI <tusami@gmail.com>
2025-12-25 15:38:02 +0900

    DeleteDiaryUseCase.kt 不要なTODOコメントを削除

--- app/src/main/java/com/websarva/wings/android/zuboradiary/domain/usecase/diary/DeleteDiaryUseCase.kt
+++ app/src/main/java/com/websarva/wings/android/zuboradiary/domain/usecase/diary/DeleteDiaryUseCase.kt
@@ -77,15 +77,6 @@ internal class DeleteDiaryUseCase @Inject constructor(
                     DiaryDeleteException.Unknown(e)
                 )
             } catch (e: DomainException) {
-                // TODO:下記仕様で問題ないか後で検討。
-                //      現在考えれる問題点
-                //      - 画像ファイル名が日記IDで構成されている為、ユーザーが手動で削除するのは難しい。
-                //      - 画像ファイル名に日記日付を加えると画像添付もとの日記の日付が変更された時、
-                //        画像ファイル名も変更する必要がある。
-                //        (データベースの日記データの添付画像ファイル名も変更する必要が出てくる)
-                // MEMO:本アプリの日記は一日一件のみ保存が可能となる仕様のため、画像ファイル削除失敗が続くと、
-                //      その日付に対しての新しい日記を作成できなくなる。
-                //      そのためロールバック処理を廃止し、画像ファイル削除失敗をユーザーに通知するのみとする。
                 Log.e(logTag, "${logMsg}警告_画像ファイル削除エラー", e)
                 return UseCaseResult.Failure(
                     DiaryDeleteException.ImageFileDeleteFailure(
