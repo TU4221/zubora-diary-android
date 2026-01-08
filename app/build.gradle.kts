6edb30cccb59a3dcdbe5918b3a5c8d4525e79658
Takuya USAMI <tusami@gmail.com>
2026-01-09 00:43:54 +0900

    未使用ライブラリ `play-services-contextmanager` の依存関係を削除

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -88,7 +88,6 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.kotlinx.coroutines.play.services)
-    implementation(libs.play.services.contextmanager)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
